783258604c44bd39aaa8e5e0f8d5c02ecf2539f4
nikhiltri <me@nikhiltrivedi.com>
2017-06-08 23:14:22 -0500

    Set up routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,22 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function()
+{
+
+    Route::any('/artworks', 'ArtworksController@respondMethodNotAllowed');
+    Route::get('/artworks', 'ArtworksController@index');
+
+    Route::any('/artworks/{artwork}', 'ArtworksController@respondMethodNotAllowed');
+    Route::get('/artworks/{artwork}', 'ArtworksController@show');
+
+    Route::any('/artworks/{artwork}/artist', 'ArtworksController@respondMethodNotAllowed');
+    Route::get('/artworks/{artwork}/artist', 'ArtistsController@index');
+
+    Route::any('/artworks/{artwork}/department', 'ArtworksController@respondMethodNotAllowed');
+    Route::get('/artworks/{artwork}/department', 'DepartmentsController@index');
+
+    Route::resource('/artists', 'ArtistsController', ['only' => ['index', 'show']]);
+    Route::resource('/departments', 'DepartmentsController', ['only' => ['index', 'show']]);
+
 });
