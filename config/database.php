bd56f287bbd0fda2da0f50ad048c6dfca112e9ac
nikhiltri <ntrivedi@artic.edu>
2017-06-22 15:29:16 -0500

    Remove prefixes from Collections tables

--- config/database.php
+++ config/database.php
@@ -49,24 +49,9 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
+            'strict' => true,
+            'engine' => null,
             'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-
-        'mysql_for_citilake_data' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => 'citilake_',
-            'strict' => true,
-            'engine' => null,
         ],
 
         'pgsql' => [
