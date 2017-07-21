a650298d945edaaf24ac6a7f34aab84d2e02460b
Illya Moskvin <imoskvin@artic.edu>
2017-07-21 18:24:10 -0500

    Make Collections\Category searchable
    php artisan migrate:refresh
    php artisan test:search CategoriesTableSeeder

--- app/Models/Collections/Category.php
+++ app/Models/Collections/Category.php
@@ -3,9 +3,12 @@
 namespace App\Models\Collections;
 
 use App\Models\CollectionsModel;
+use App\Models\SolrSearchable;
 
 class Category extends CollectionsModel
 {
+
+    use SolrSearchable;
 
     protected $primaryKey = 'citi_id';
     protected $dates = ['source_created_at', 'source_modified_at', 'source_indexed_at'];
