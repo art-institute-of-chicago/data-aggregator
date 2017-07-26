e27e3e03e15fa2947215d26a76b9db8e0840df9c
nikhiltri <ntrivedi@artic.edu>
2017-07-25 21:58:40 -0500

    Add Dsc Publications to Solr import

--- app/Models/Dsc/Publication.php
+++ app/Models/Dsc/Publication.php
@@ -10,4 +10,20 @@ class Publication extends DscModel
 
     use SolrSearchable;
 
+
+    /**
+     * Turn this model object into a generic array.
+     *
+     * @param boolean  $withTitles
+     * @return array
+     */
+    public function transformFields()
+    {
+
+        return [
+            'link' => $this->link,
+        ];
+
+    }
+
 }
