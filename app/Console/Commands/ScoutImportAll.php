8ba5ab08baa695ffe72ccbaf41fb5ec5894b2bdf
nikhiltri <ntrivedi@artic.edu>
2017-07-26 21:58:49 -0500

    Show output for import all to Solr console command

--- app/Console/Commands/ScoutImportAll.php
+++ app/Console/Commands/ScoutImportAll.php
@@ -38,31 +38,31 @@ class ScoutImportAll extends Command
     public function handle()
     {
 
-        \Artisan::call("scout:import", ['model' => \App\Models\Collections\Agent::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Collections\Department::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Collections\Category::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Collections\Gallery::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Collections\Artwork::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Collections\Link::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Collections\Sound::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Collections\Video::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Collections\Text::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Collections\Exhibition::class]);
+        $this->call("scout:import", ['model' => \App\Models\Collections\Agent::class]);
+        $this->call("scout:import", ['model' => \App\Models\Collections\Department::class]);
+        $this->call("scout:import", ['model' => \App\Models\Collections\Category::class]);
+        $this->call("scout:import", ['model' => \App\Models\Collections\Gallery::class]);
+        $this->call("scout:import", ['model' => \App\Models\Collections\Artwork::class]);
+        $this->call("scout:import", ['model' => \App\Models\Collections\Link::class]);
+        $this->call("scout:import", ['model' => \App\Models\Collections\Sound::class]);
+        $this->call("scout:import", ['model' => \App\Models\Collections\Video::class]);
+        $this->call("scout:import", ['model' => \App\Models\Collections\Text::class]);
+        $this->call("scout:import", ['model' => \App\Models\Collections\Exhibition::class]);
 
-        \Artisan::call("scout:import", ['model' => \App\Models\Shop\Category::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Shop\Product::class]);
+        $this->call("scout:import", ['model' => \App\Models\Shop\Category::class]);
+        $this->call("scout:import", ['model' => \App\Models\Shop\Product::class]);
 
-        \Artisan::call("scout:import", ['model' => \App\Models\Membership\Event::class]);
+        $this->call("scout:import", ['model' => \App\Models\Membership\Event::class]);
 
-        \Artisan::call("scout:import", ['model' => \App\Models\Mobile\Tour::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Mobile\TourStop::class]);
+        $this->call("scout:import", ['model' => \App\Models\Mobile\Tour::class]);
+        $this->call("scout:import", ['model' => \App\Models\Mobile\TourStop::class]);
 
-        \Artisan::call("scout:import", ['model' => \App\Models\Dsc\Publication::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Dsc\Section::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Dsc\WorkOfArt::class]);
-        \Artisan::call("scout:import", ['model' => \App\Models\Dsc\Collector::class]);
+        $this->call("scout:import", ['model' => \App\Models\Dsc\Publication::class]);
+        $this->call("scout:import", ['model' => \App\Models\Dsc\Section::class]);
+        $this->call("scout:import", ['model' => \App\Models\Dsc\WorkOfArt::class]);
+        $this->call("scout:import", ['model' => \App\Models\Dsc\Collector::class]);
 
-        \Artisan::call("scout:import", ['model' => \App\Models\StaticArchive\Site::class]);
+        $this->call("scout:import", ['model' => \App\Models\StaticArchive\Site::class]);
 
     }
 }
