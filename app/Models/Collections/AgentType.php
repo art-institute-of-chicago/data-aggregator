e5e96453506b11fecccbcb9e3be8a27c9d320708
Illya Moskvin <imoskvin@artic.edu>
2017-07-31 11:13:51 -0500

    Fix 500 error due to AgentType transformation

--- app/Models/Collections/AgentType.php
+++ app/Models/Collections/AgentType.php
@@ -18,15 +18,8 @@ class AgentType extends CollectionsModel
     public function transformFields()
     {
 
-        return [
-            'birth_date' => $this->birth_date,
-            'birth_place' => $this->birth_place,
-            'death_date' => $this->death_date,
-            'death_place' => $this->death_place,
-            'is_licensing_restricted' => (bool) $this->licensing_restricted,
-            'agent_type' => $this->agentType()->getResults() ? $this->agentType()->getResults()->title : '',
-            'agent_type_id' => $this->agent_type_citi_id,
-        ];
+        // AgentType has no non-id, non-date fields
+        return [];
 
     }
 
