3a04ba4cd06ce8276c9e9e3ccefa3a41bf3bfa9b
Illya Moskvin <imoskvin@artic.edu>
2017-08-09 17:02:24 -0500

    Refactor SearchController
    We now differentiate between three search modes: (1) empty search,
    (2) simple search, and (3) full search. All three modes are still
    entered through a single route endpoint. Which mode is followed is
    determined through the Input parameters passed by the user:
    
     1. All modes support simple pagination w/ `from` and `size`
        Later, pagination could be updated to match Laravel's conventions
    
     2. Suggestions are not applied to empty search.
    
     3. Everything is wrapped in a `bool` query:
    
        a. Our "magic" boosts go into `bool.should` array
        b. User-specified queries are wrapped into `bool.must` array
    
     4. This thin API accepts a (limited) subset of parameters commonly
        passed to the ES Search API, esp. via its "Request Body Search"
    
     5. The `q` param is always required for non-empty search, even if
        the user passes a full `query`. This is contrary to convention,
        but we need it for search suggestions, etc.
    
        `q` is not meant for Lucene syntax, only simple search strings.
    
    Ideally, this will allow others to use our API via ES's official
    clients without encountering UnexpectedValueException. Generally,
    it seems desirable to follow ES's and Laravel's conventions.

--- app/Http/Controllers/Search/SearchController.php
+++ app/Http/Controllers/Search/SearchController.php
@@ -30,6 +30,43 @@ class SearchController extends Controller
 
 
     /**
+     * List of allowed Input params for querying.
+     *
+     * @var array
+     */
+    private $allowed = [
+
+        // TODO: `type` handling
+
+        // Required: we must know the core search string
+        // We use `q` b/c it won't cause UnexpectedValueException, if the user uses an official ES Client
+        'q',
+
+        // Complex query mode
+        'query',
+        'sort',
+
+        // Pagination-related stuff
+        // TODO: Match Laravel's pagination conventions?
+        'from',
+        'size',
+
+        // Currently unsupported by the official ES PHP Client
+        // 'search_after',
+
+        // Choose which fields to return
+        '_source',
+
+        // TODO: Hide implementation by combining _source w/ other fields?
+        // Note that _source supports wildcards, while the others do not
+        // 'fields', // old convention
+        // 'stored_fields',
+        // 'docvalue_fields',
+
+    ];
+
+
+    /**
      * Create a new controller instance.
      *
      * @return void
@@ -41,94 +78,219 @@ class SearchController extends Controller
 
 
     /**
-     * General search for keyword or phrase.
+     * General entry point for search. There are three modes:
+     *
+     * 1. Don't pass any params to view all works, magically sorted.
+     * 2. Pass `q` param w/ string for a simple, optimized search.
+     * 3. Pass `q` param *and* subset of ES Request Body params.
+     *
+     * @link https://www.elastic.co/guide/en/elasticsearch/reference/current/search-request-body.html
      *
      * @return void
      */
     public function search()
     {
 
+        // Strip down the (top-level) params to what our thin client supports
+        $input = $this->getValidInput();
+
+        // Build the basic scaffolding used by all ES queries
         $params = [
+
             'index' => $this->index,
             'type' => null, // search all types
-            // TODO: Consider using json for body for cross-compatibility?
+
+            'from' => $input['from'],
+            'size' => $input['size'],
+
+            // TODO: Re-enable this once the official ES PHP Client supports it
+            // 'search_after' => $input['search_after'],
+
             'body' => [
+
                 'query' => [
                     'bool' => [
-                        'must' => [
-                            [
-                                'multi_match' => [
-                                    'query' => Input::get('q', ''),
-                                    'fuzziness' => 3,
-                                    'prefix_length' => 1,
-                                    'fields' => [
-                                        '_all',
-                                    ]
-                                ]
-                            ],
-                        ],
-                        'should' => [
-                            [
-                                // Boost essential works
-                                'terms' => [
-                                    'id' => Artwork::getEssentialIds()
-                                ]
-                            ]
-                        ],
-                        'filter' => Input::get('filter', null),
+                        'must' => [], // user-specified queries go here
+                        'should' => [], // our custom boosting queries go here
                     ]
                 ],
-                'suggest' => [
-                    'text' => Input::get('q', ''),
-                    'autocomplete' =>[
-                        'prefix' => Input::get('q', ''),
-                        'completion' => [
-                            'field' => 'suggest',
-                        ],
-                    ],
 
-                    // This is currently not working
-                    'phrase-suggest' => [
-                        'phrase' => [
-                            'field' => 'title.trigram',
-                            'gram_size' => 3,
-                            'direct_generator' => [
-                                [
-                                    'field' => 'title.trigram',
-                                    'suggest_mode' => 'always'
-                                ],
-                                [
-                                    'field' => 'title.reverse',
-                                    'suggest_mode' => 'always',
-                                    'pre_filter' => 'reverse',
-                                    'post_filter' => 'reverse'
-                                ],
-                            ],
-                            'highlight' => [
-                                'pre_tag' => '<em>',
-                                'post_tag' => '</em>'
-                            ],
-                        ],
-                    ],
-
-                    // This works, but is not very usable for API consumers
-                    'term-suggest' => [
-                        'term' => [
-                            'field' => 'title'
-                        ],
-                    ],
-                ],
             ],
+
         ];
 
-        $response = Elasticsearch::search( $params );
+        if( is_null( $input['q'] ) ) {
+
+            // Empy search requires special handling, e.g. no suggestions
+            $params = $this->getEmptySearchParams( $params );
+
+        } else {
+
+            if( is_null( $input['query'] ) ) {
+
+                $params = $this->getSimpleSearchParams( $params, $input );
+
+            } else {
+
+                $params = $this->getFullSearchParams( $params, $input );
+
+            }
+
+            // Both `query` and `q`-only searches support suggestions
+            $params = $this->getSuggestSearchParams( $params, $input );
+
+        }
+
+        // Boost essential works
+        // TODO: Move this to separate function once additional boosts are required
+        $params['body']['query']['bool']['should'][] = [
+            'terms' => [
+                'id' => Artwork::getEssentialIds()
+            ]
+        ];
+
+        // Keeping this here for debug purposes:
+        // return response()->json( $params );
+
+        try {
+            $response = Elasticsearch::search( $params );
+        } catch (\Exception $e) {
+            return response( $e->getMessage(), $e->getCode() )->header('Content-Type', 'application/json');
+        }
+
+        // This is the actual query sent by the official ES PHP client
+        // return response()->json( json_decode( Elasticsearch::connection('default')->transport->lastConnection->getLastRequestInfo()['request']['body'] ) );
 
         $ret = [];
         $ret = $this->addTotal($ret, $response);
         $ret = $this->addData($ret, $response);
-        $ret = $this->addSuggest($ret, $response);
+
+        if( !is_null( $input['q'] ) ) {
+            $ret = $this->addSuggest($ret, $response);
+        }
 
         return $ret;
+
+    }
+
+
+    private function getValidInput( ) {
+
+        // List of allowed user-specified params
+        $allowed = $this->allowed;
+
+        // `null` will be the default value for all params
+        $defaults = array_fill_keys( $allowed, null );
+
+        // Grab all user input (query string params or json)
+        $input = Input::all();
+
+        // Reduce the input set to the params we allow
+        $input = array_intersect_key($input, array_flip( $allowed ) );
+
+        // Combine $defaults and $input: we won't have to use is_set, only is_null
+        $input = array_merge( $defaults, $input );
+
+        return $input;
+
+    }
+
+
+    private function getEmptySearchParams( $params ) {
+
+        // PHP JSON-encodes empty array as [], not {}
+        $params['body']['query']['bool']['must'][] = [
+            'match_all' => new \stdClass()
+        ];
+
+        return $params;
+
+    }
+
+
+    private function getSimpleSearchParams( $params, $input ) {
+
+        // TODO: Determine if defaults for `fuzziness` and `prefix_length` are sufficient
+        // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-fuzzy-query.htm
+
+        // TODO: Determine which fields to query w/ is_numeric()?
+        // See also `lenient` param
+
+        $params['body']['query']['bool']['must'][] = [
+            'multi_match' => [
+                'query' => $input['q'],
+                'fuzziness' => 3,
+                'prefix_length' => 1,
+                'fields' => [
+                    '_all',
+                ]
+            ]
+        ];
+
+        return $params;
+
+    }
+
+
+    private function getFullSearchParams( $params, $input ) {
+
+        // TODO: Validate `query` input to reduce shenanigans
+        $params['body']['query']['bool']['must'][] = $input['query'];
+
+        // TODO: Deep-find `fields` in certain queries + replace them w/ our custom field list
+
+        return $params;
+
+    }
+
+
+    private function getSuggestSearchParams( $params, $input ) {
+
+        $params['body']['suggest'] = [
+
+            'text' => $input['q'],
+
+            'autocomplete' =>[
+                'prefix' =>  $input['q'],
+                'completion' => [
+                    'field' => 'suggest',
+                ],
+            ],
+
+            // This is currently not working
+            'phrase-suggest' => [
+                'phrase' => [
+                    'field' => 'title.trigram',
+                    'gram_size' => 3,
+                    'direct_generator' => [
+                        [
+                            'field' => 'title.trigram',
+                            'suggest_mode' => 'always'
+                        ],
+                        [
+                            'field' => 'title.reverse',
+                            'suggest_mode' => 'always',
+                            'pre_filter' => 'reverse',
+                            'post_filter' => 'reverse'
+                        ],
+                    ],
+                    'highlight' => [
+                        'pre_tag' => '<em>',
+                        'post_tag' => '</em>'
+                    ],
+                ],
+            ],
+
+            // This works, but is not very usable for API consumers
+            'term-suggest' => [
+                'term' => [
+                    'field' => 'title'
+                ],
+            ],
+
+        ];
+
+        return $params;
 
     }
 
