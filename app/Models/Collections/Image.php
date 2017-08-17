aab9b1084dc555564f4bc290ae3b00010c64a90b
Illya Moskvin <imoskvin@artic.edu>
2017-08-17 12:11:58 -0500

    Add artwork_titles to Images for search

--- app/Models/Collections/Image.php
+++ app/Models/Collections/Image.php
@@ -16,4 +16,27 @@ class Image extends Asset
 
     // TODO: Attach artworks' titles to the Image for search
 
+    /**
+     * Turn this model object into a generic array.
+     *
+     * @return array
+     */
+    public function transformFields()
+    {
+
+        // Image currently extends Asset, so it contains the following fields:
+        // description, content, artist, artist_id
+
+        // However, these fields are "legacy" leftovers from Interpretive Resource
+        // They will likely *not* be filled during the upcoming LPM Enhancements work
+
+        // THerefore, we exclude them from this transformation
+
+        return array_merge(
+            [
+                'artwork_titles' => $this->artworks()->pluck('title'),
+            ]
+        );
+    }
+
 }
