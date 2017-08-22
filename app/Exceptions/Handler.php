341dd23456de528bf83567c83412131ddef36032
Illya Moskvin <imoskvin@artic.edu>
2017-08-22 11:09:48 -0500

    Render exceptions with JSON for API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -40,15 +40,41 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Exception $e
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Exception $e)
     {
-        if ($exception instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
-            return response()->error('Resouce not found', 'Resource not found.', Response::HTTP_NOT_FOUND);
+        // If the request wants JSON (AJAX doesn't always want JSON)
+        if ($request->wantsJson()) {
+            // Define the response
+            $response = [
+                'errors' => 'Sorry, something went wrong.'
+            ];
+
+            // If the app is in debug mode
+            if (config('app.debug')) {
+                // Add the exception class name, message and stack trace to response
+                $response['exception'] = get_class($e); // Reflection might be better here
+                $response['message'] = $e->getMessage();
+                $response['trace'] = $e->getTrace();
+            }
+
+            // Default response of 400
+            $status = 400;
+
+            // If this exception is an instance of HttpException
+            if ($this->isHttpException($e)) {
+                // Grab the HTTP status code from the Exception
+                $status = $e->getStatusCode();
+            }
+
+            // Return a JSON response with the response array and status code
+            return response()->json($response, $status);
         }
-        return parent::render($request, $exception);
+
+        // Default to the parent class' implementation of handler
+        return parent::render($request, $e);
     }
 
     /**
