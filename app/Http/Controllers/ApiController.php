db7ec2d6bc405025d39a33e8d198a361a7949c72
Illya Moskvin <imoskvin@artic.edu>
2017-08-30 16:44:39 -0500

    Refactor EventsController into ApiNewController
    This is the first step towards reducing code duplication. The method
    here is similar to the one used in `data-service-events`. We created
    a transitional ApiNewController that will eventually replace the old
    one. Some changes:
    
     - Remove obsolete `use` operators
     - Move error definitions to the bottom
     - Make `ApiController` an `abstract` class

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,72 +2,69 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Collections\Artwork;
-use App\Http\Transformers\ApiSerializer;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use League\Fractal\Manager;
 
-class ApiController extends Controller
+abstract class ApiController extends Controller
 {
-
-    public $manager;
 
     const LIMIT_MAX = 1000;
 
-    public function respondNotFound($message = 'Not found', $detail = 'The item you requested cannot be found.')
-    {
-        return response()->error($message, $detail, Response::HTTP_NOT_FOUND);
-    }
 
-    public function respondInvalidSyntax($message = 'Invalid syntax', $detail = 'The identifier is invalid.')
-    {
-        return response()->error($message, $detail, Response::HTTP_BAD_REQUEST);
-    }
-
-    public function respondFailure($message = 'Failed request', $detail = 'The request failed.')
-    {
-        return response()->error($message, $detail, Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    public function respondForbidden($message = 'Forbidden', $detail = 'This request is forbidden.')
-    {
-        return response()->error($message, $detail, Response::HTTP_FORBIDDEN);
-    }
-
-    public function respondTooManyIds($message = 'Invalid number of ids', $detail = 'You have requested too many ids. Please send a smaller amount.')
-    {
-        return response()->error($message, $detail, Response::HTTP_FORBIDDEN);
-    }
-
-    public function respondMethodNotAllowed($message = 'Method not allowed', $detail = 'Method not allowed.')
-    {
-        return response()->error($message, $detail, Response::HTTP_METHOD_NOT_ALLOWED);
-    }
-
-    public function isUuid($id)
+    private function isUuid($id)
     {
 
         // We must not be using UUIDv3, since the typical regex wasn't matching
         $uuid = '/^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i';
 
-        if (preg_match($uuid, $id))
-        {
-            return true;
-        }
-        return false;
+        return preg_match($uuid, $id);
+
     }
 
-    public function isDscId($id)
+    private function isDscId($id)
     {
 
         $dscFormat = '/^[a-z]{2,3}-[0-9]+-[0-9]+$/i';
 
-        if (preg_match($dscFormat, $id))
-        {
-            return true;
-        }
-        return false;
+        return preg_match($dscFormat, $id);
+
+    }
+
+    // See boot() in App\Providers\AppServiceProvider for the error() macro
+    // TODO: Move these into Exceptions?
+
+    private function respondNotFound($message = 'Not found', $detail = 'The item you requested cannot be found.')
+    {
+        return response()->error($message, $detail, Response::HTTP_NOT_FOUND);
+    }
+
+    private function respondInvalidSyntax($message = 'Invalid syntax', $detail = 'The identifier is invalid.')
+    {
+        return response()->error($message, $detail, Response::HTTP_BAD_REQUEST);
+    }
+
+    private function respondFailure($message = 'Failed request', $detail = 'The request failed.')
+    {
+        return response()->error($message, $detail, Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    private function respondForbidden($message = 'Forbidden', $detail = 'This request is forbidden.')
+    {
+        return response()->error($message, $detail, Response::HTTP_FORBIDDEN);
+    }
+
+    private function respondTooManyIds($message = 'Invalid number of ids', $detail = 'You have requested too many ids. Please send a smaller amount.')
+    {
+        return response()->error($message, $detail, Response::HTTP_FORBIDDEN);
+    }
+
+    private function respondBigLimit($message = 'Invalid limit', $detail = 'You have requested too many resources. Please set a smaller limit.')
+    {
+        return $this->error($message, $detail, Response::HTTP_FORBIDDEN);
+    }
+
+    private function respondMethodNotAllowed($message = 'Method not allowed', $detail = 'Method not allowed.')
+    {
+        return response()->error($message, $detail, Response::HTTP_METHOD_NOT_ALLOWED);
     }
 
 }
---
+++ app/Http/Controllers/ApiNewController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+abstract class ApiNewController extends ApiController
+{
+
+    protected $model;
+
+    protected $transformer;
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Collections\Event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, $eventId)
+    {
+
+        // Technically this will never be called, b/c we only bind Route.get
+        if ($request->method() != 'GET')
+        {
+            return $this->respondMethodNotAllowed();
+        }
+
+        // Only allow numeric ids
+        if (intval($id) <= 0)
+        {
+            return $this->respondInvalidSyntax();
+        }
+
+        // TODO: Improve exception handling via Handler
+        $item = ($this->model)::find($id);
+
+        if (!$item)
+        {
+            return $this->respondNotFound();
+        }
+
+        return response()->item($item, new $this->transformer);
+
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param null $id
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+
+        // Technically this will never be called, b/c we only bind Route.get
+        if ($request->method() != 'GET')
+        {
+            return $this->respondMethodNotAllowed();
+        }
+
+        // Process ?ids= query param
+        $ids = $request->input('ids');
+
+        if ($ids)
+        {
+            return $this->showMutliple($ids);
+        }
+
+        // Check if the ?limit= is too big
+        $limit = $request->input('limit') ?: 12;
+
+        if ($limit > static::LIMIT_MAX)
+        {
+            return $this->respondBigLimit();
+        }
+
+        // Assumes the inheriting class set model and transformer
+        $all = ($this->model)::paginate($limit);
+
+        return response()->collection($all, new $this->transformer);
+
+
+    }
+
+    /**
+     * Display multiple resources.
+     *
+     * @param string $ids
+     * @return \Illuminate\Http\Response
+     */
+    private function showMutliple($ids = '')
+    {
+
+        $ids = explode(',',$ids);
+
+        if (count($ids) > static::LIMIT_MAX)
+        {
+            return $this->respondTooManyIds();
+        }
+
+        $all = ($this->model)::find($ids);
+
+        return response()->collection($all, new $this->transformer);
+
+    }
+
+
+}
