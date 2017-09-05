489024f4aa0acccb3023fd0fbf88f2dba421e569
Illya Moskvin <imoskvin@artic.edu>
2017-09-05 16:28:30 -0500

    Update Event model and migration with new schema

--- app/Models/Membership/Event.php
+++ app/Models/Membership/Event.php
@@ -10,8 +10,33 @@ class Event extends MembershipModel
 
     use ElasticSearchable;
 
-    protected $dates = ['start', 'end', 'on_sale', 'off_sale', 'source_created_at', 'source_modified_at', 'source_indexed_at'];
+    protected $dates = [
+        'start_at',
+        'end_at',
+        'source_created_at',
+        'source_modified_at',
+    ];
 
+    protected function getFillFieldsFrom($source)
+    {
+
+        return [
+
+            'type_id' => $source->type_id,
+
+            'start_at' => strtotime($source->start_at),
+            'end_at' => strtotime($source->end_at),
+
+            'resource_id' => $source->resource_id,
+            'resource_title' => $source->resource_title,
+
+            'is_after_hours' => $source->is_after_hours,
+            'is_private_event' => $source->is_private_event,
+            'is_admission_required' => $source->is_admission_required,
+
+        ];
+
+    }
 
     /**
      * Turn this model object into a generic array.
@@ -23,20 +48,21 @@ class Event extends MembershipModel
     {
 
         return [
-            'start' => $this->start->toIso8601String(),
-            'end' => $this->end->toIso8601String(),
-            'type' => $this->type,
-            'on_sale' => $this->on_sale->toIso8601String(),
-            'off_sale' => $this->off_sale->toIso8601String(),
-            'resource' => $this->resource,
-            'user_event_number' => $this->user_event_number,
-            'available' => $this->available,
-            'total_capacity' => $this->total_capacity,
-            'status' => $this->status,
-            'rs_event_seat_map_id' => $this->rs_event_seat_map_id,
-            'has_roster' => (bool) $this->has_roster,
-            'is_private_event' => (bool) $this->private_event,
-            'has_holds' => (bool) $this->has_holds,
+
+            'type_id' => $this->type_id,
+
+            'start_at' => $this->start_at->toIso8601String(),
+            'end_at' => $this->end_at->toIso8601String(),
+
+            'resource_id' => $this->resource_id,
+            'resource_title' => $this->resource_title,
+
+            // Caution: (bool) null = false
+            // TODO: Use $casts throughout the codebase
+            'is_after_hours' => (bool) $this->is_after_hours,
+            'is_private_event' => (bool) $this->is_private_event,
+            'is_admission_required' => (bool) $this->is_admission_required,
+
         ];
 
     }
@@ -50,51 +76,34 @@ class Event extends MembershipModel
     public function elasticsearchMappingFields()
     {
 
-        return
-            [
-                'start' => [
-                    'type' => 'date',
-                ],
-                'end' => [
-                    'type' => 'date',
-                ],
-                'type' => [
-                    'type' => 'keyword',
-                ],
-                'on_sale' => [
-                    'type' => 'date',
-                ],
-                'off_sale' => [
-                    'type' => 'date',
-                ],
-                'resource' => [
-                    'type' => 'integer',
-                ],
-                'user_event_number' => [
-                    'type' => 'integer',
-                ],
-                'available' => [
-                    'type' => 'integer',
-                ],
-                'total_capacity' => [
-                    'type' => 'integer',
-                ],
-                'status' => [
-                    'type' => 'integer',
-                ],
-                'rs_event_seat_map_id' => [
-                    'type' => 'integer',
-                ],
-                'has_roster' => [
-                    'type' => 'boolean',
-                ],
-                'is_private_event' => [
-                    'type' => 'boolean',
-                ],
-                'has_holds' => [
-                    'type' => 'boolean',
-                ],
-            ];
+        return [
+
+            'type_id' => [
+                'type' => 'keyword',
+            ],
+            'start_at' => [
+                'type' => 'date',
+            ],
+            'end_at' => [
+                'type' => 'date',
+            ],
+            'resource_id' => [
+                'type' => 'integer',
+            ],
+            'resource_title' => [
+                'type' => 'keyword',
+            ],
+            'is_after_hours' => [
+                'type' => 'boolean',
+            ],
+            'is_private_event' => [
+                'type' => 'boolean',
+            ],
+            'is_admission_required' => [
+                'type' => 'boolean',
+            ],
+
+        ];
 
     }
 
