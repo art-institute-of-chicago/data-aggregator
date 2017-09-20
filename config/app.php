41b264213f2f36b9e642f1fdb9a5ac3caa299d77
Illya Moskvin <imoskvin@artic.edu>
2017-09-20 11:55:23 -0500

    Remove BroadcastServiceProvider and its configs
    See also 71124f1. We aren't (and won't?) be using WebSockets, so this
    is unnecessary. If the situation changes, we can restore these files.

--- config/app.php
+++ config/app.php
@@ -141,7 +141,7 @@ return [
          * Laravel Framework Service Providers...
          */
         Illuminate\Auth\AuthServiceProvider::class,
-        Illuminate\Broadcasting\BroadcastServiceProvider::class,
+        // Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
@@ -173,7 +173,6 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
