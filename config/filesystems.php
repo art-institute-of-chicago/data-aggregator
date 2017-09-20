362c2792e0ec03a575f4db1ec728527a4915c5ff
Illya Moskvin <imoskvin@artic.edu>
2017-09-20 15:31:04 -0500

    Remove public filesystem definition
    I think we won't be needing to store publicly-accessible files.
    I think the visibility public is what's key here. This change
    should be reverted if the situation changes.

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,13 +48,6 @@ return [
             'root' => storage_path('app'),
         ],
 
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
-        ],
-
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_KEY'),
