5fc0f58138a66ac42df5ebb1e9a0578f456e773b
nikhiltri <ntrivedi@artic.edu>
2017-09-20 15:57:39 -0500

    Remove condition that prevents imports if table has data in it.

--- app/Console/Commands/ImportCollectionsFull.php
+++ app/Console/Commands/ImportCollectionsFull.php
@@ -95,8 +95,10 @@ class ImportCollectionsFull extends AbstractImportCommand
 
         $model = \App\Models\CollectionsModel::classFor($endpoint);
 
-        // Abort if the table is already filled
-        if( $model::count() > 0 )
+        // Abort if the table is already filled in production.
+        // In test we want to update existing records. Once we verify this
+        // functionality we may want to take this condition completely out.
+        if( $model::count() > 0 && config('app.env') == 'production')
         {
             return false;
         }
