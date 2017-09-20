86d64689ac2973e03aa220f96c9ded67a737c5eb
Illya Moskvin <imoskvin@artic.edu>
2017-09-20 10:07:19 -0500

    Remove VerifyCsrfToken middleware
    I think we can remove this for now, since none of our routes trigger
    anything that writes to a database. We can revisit this later.
    
    Plus, we aren't using the `web` route group currently, so it wasn't
    being triggered. From that perspective, everything relating to the
    `web` route group could be removed without breaking the app.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,7 +33,6 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
