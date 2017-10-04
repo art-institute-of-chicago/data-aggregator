f4eb3abb4f5a91a00fba856f8274e18bfc6214d2
Illya Moskvin <imoskvin@artic.edu>
2017-10-04 18:14:47 -0500

    Fix id validation for Footnotes

--- app/Http/Controllers/FootnotesController.php
+++ app/Http/Controllers/FootnotesController.php
@@ -9,4 +9,18 @@ class FootnotesController extends ApiController
 
     protected $transformer = \App\Http\Transformers\FootnoteTransformer::class;
 
+    /**
+     * Validate `id` route or query string param format.
+     * Footnotes follow OSCI `id` format, e.g. `fn-429-158`
+     *
+     * @param mixed $id
+     * @return boolean
+     */
+    protected function validateId( $id )
+    {
+
+        return preg_match( '/^fn-[0-9]+-[0-9]+$/', $id );
+
+    }
+
 }
