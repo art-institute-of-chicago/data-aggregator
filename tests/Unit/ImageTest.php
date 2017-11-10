05f2626e317b3ab3d817871bfcc1030aaa8a58cb
Illya Moskvin <imoskvin@artic.edu>
2017-11-10 10:50:15 -0600

    Delete commented-out tests for image handling
    The data hub will not be used for image upload for the foreseeable
    future. I wanted to make this a dedicated commit, so that its easier
    to reference (and potentially reverse) later.

--- tests/Unit/ImageTest.php
+++ tests/Unit/ImageTest.php
@@ -11,49 +11,4 @@ class ImageTest extends AssetTestCase
 
     protected $route = 'images';
 
-
-    // /** @test */
-    // public function it_deletes_a_single_image()
-    // {
-
-    //     $this->make(Image::class);
-
-    //     $response = $this->deleteJson('api/v1/images/' .$this->ids[0]);
-    //     $response->assertSuccessful();
-
-    // }
-
-    // /** @test */
-    // public function it_deletes_multiple_images()
-    // {
-
-    //     $this->times(5)->make(Image::class);
-
-    //     $response = $this->deleteJson('api/v1/images?ids=' .implode(',',array_slice($this->ids, 0, 3)));
-    //     $response->assertSuccessful();
-
-    // }
-
-    // /** @test */
-    // public function it_overwrites_an_existing_image()
-    // {
-
-    //     $this->make(Image::class);
-
-    //     $response = $this->putJson('api/v1/images/' .$this->ids[0]);
-    //     $response->assertSuccessful();
-
-    // }
-
-    // /** @test */
-    // public function it_adds_a_new_image()
-    // {
-
-    //     $this->make(Image::class);
-
-    //     $response = $this->postJson('api/v1/images');
-    //     $response->assertSuccessful();
-
-    // }
-
 }
