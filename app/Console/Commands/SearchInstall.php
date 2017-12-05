d0ceda45b8f440a6dc10fab70207243221c6484a
Illya Moskvin <imoskvin@artic.edu>
2017-12-05 10:31:29 -0600

    Enable SearchInstall to create a single index
    Useful for debugging a single model.

--- app/Console/Commands/SearchInstall.php
+++ app/Console/Commands/SearchInstall.php
@@ -14,7 +14,8 @@ class SearchInstall extends Command
     use Indexer;
 
     protected $signature = 'search:install
-                            {index? : The name of the index to create. Will be created as an alias to a set of indexes with the same prefixes}
+                            {prefix? : Prefix for index(es) and the name of the alias to access them}
+                            {model? : Create one index for specific model and tie it to this alias}
                             {--y|yes : Answer "yes" to all prompts confirming to delete index}';
 
     protected $description = 'Set up the Search Service indexes with data types and fields';
@@ -23,37 +24,54 @@ class SearchInstall extends Command
     public function handle()
     {
 
-        $prefix = $this->argument('index') ?? env('ELASTICSEARCH_INDEX');
+        $prefix = $this->argument('prefix') ?? env('ELASTICSEARCH_INDEX');
 
-        $models = app('Search')->getSearchableModels();
-
-        foreach ($models as $model)
+        if ($this->argument('model'))
         {
 
-            $endpoint = endpointFor($model);
-            $index = $prefix . '-' . $endpoint;
+            $this->install( $this->argument('model'), $prefix );
 
-            if (!$this->destroy($index, $this->option('yes')))
+        } else {
+
+            $models = app('Search')->getSearchableModels();
+
+            foreach ($models as $model)
             {
 
-                $this->warn('Could not destroy index ' . $index . '. Exiting.');
-
-                return 0;
+                $this->install( $model, $index );
 
             }
 
-            $params = config('elasticsearch.indexParams');
+        }
 
-            $params['index'] = $index;
-            $params['body']['mappings'] = app('Search')->getElasticsearchMapping( $model );
+    }
 
-            $return = Elasticsearch::indices()->create($params);
 
-            $this->info($this->done($return));
+    private function install( $model, $prefix )
+    {
 
-            Artisan::call('search:alias', ['source' => $index, 'alias' => $prefix, '--single' => true]);
+        $endpoint = endpointFor($model);
+        $index = $prefix . '-' . $endpoint;
+
+        if (!$this->destroy($index, $this->option('yes')))
+        {
+
+            $this->warn('Could not destroy index ' . $index . '.');
+
+            return 0;
 
         }
+
+        $params = config('elasticsearch.indexParams');
+
+        $params['index'] = $index;
+        $params['body']['mappings'] = app('Search')->getElasticsearchMapping( $model );
+
+        $return = Elasticsearch::indices()->create($params);
+
+        $this->info($this->done($return));
+
+        Artisan::call('search:alias', ['source' => $index, 'alias' => $prefix, '--single' => true]);
 
     }
 
