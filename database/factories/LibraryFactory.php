b2ada0fdef6e94e38fbdc759dc0b21169f713fe7
nikhiltri <ntrivedi@artic.edu>
2018-02-21 09:32:05 -0600

    Add seeders and factories to library data

---
+++ database/factories/LibraryFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Library Factory
+|--------------------------------------------------------------------------
+|
+| Create models with stub data for all data coming from the Library
+| Data Service.
+|
+*/
+
+$factory->define(App\Models\Library\Material::class, function (Faker\Generator $faker) {
+    $id = '999' .'ARTIC_ALMA999' .$faker->unique()->randomNumber(8);
+    return [
+        'id' => $id,
+        'title' => ucfirst($faker->words(3, true)),
+        'date' => $faker->year(),
+    ];
+});
+
+$factory->define(App\Models\Library\Term::class, function (Faker\Generator $faker) {
+    $id = 'zz' .$faker->unique()->randomNumber(8);
+    return [
+        'id' => $id,
+        'uri' => 'http://fake.loc.fake/authorities/fake/' .$id,
+        'title' => ucfirst($faker->words(3, true)),
+    ];
+});
---
+++ database/seeds/library/LibraryDatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Library\Material;
+use App\Models\Library\Term;
+
+class LibraryDatabaseSeeder extends AbstractSeeder
+{
+
+    protected function seed()
+    {
+
+        $this->call(LibraryMaterialSeeder::class);
+        $this->call(LibraryTermSeeder::class);
+
+    }
+
+    protected static function unseed()
+    {
+
+        Material::fake()->delete();
+        Term::fake()->delete();
+
+    }
+
+}
