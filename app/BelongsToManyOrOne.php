41f195ee8e28a6826b64897586fc0f808e8e6601
Illya Moskvin <imoskvin@artic.edu>
2018-02-26 15:18:28 -0600

    Fix blank alt fields by accepting preferred null
    Definitely feeling the slowdown on `/artworks`. Might need to change
    API to be a facade on Elasticsearch for speed, if we can figure out
    `include` functionality on search.

--- app/BelongsToManyOrOne.php
+++ app/BelongsToManyOrOne.php
@@ -69,7 +69,7 @@ class BelongsToManyOrOne extends BelongsToMany
     public function isAlternative()
     {
 
-        return $this->wherePivot('preferred', '=', false)->expectMany();
+        return $this->wherePivotIn('preferred', [false, null], 'or')->expectMany();
 
     }
 
