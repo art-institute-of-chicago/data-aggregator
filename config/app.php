ca0f9e49cbb72d9125c6b35eef2b981b0eea92cb
Illya Moskvin <imoskvin@artic.edu>
2018-03-21 16:19:55 -0500

    Add `url` to `config/app.php` for Scout
    https://github.com/art-institute-of-chicago/data-hub-foundation/commit/a89a276

--- config/app.php
+++ config/app.php
@@ -166,4 +166,17 @@ return [
 
     'env' => env('APP_ENV', 'production'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Application URL
+    |--------------------------------------------------------------------------
+    |
+    | This URL is used by the console to properly generate URLs when using
+    | the Artisan command line tool. You should set this to the root of
+    | your application so that it is used when running Artisan tasks.
+    |
+    */
+
+    'url' => env('APP_URL', 'http://localhost'),
+
 ];
