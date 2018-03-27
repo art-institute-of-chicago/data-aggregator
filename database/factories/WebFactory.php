70f0e88addeb1eed8123254e7a04d255fb7db4ba
nikhiltri <ntrivedi@artic.edu>
2018-03-27 14:36:12 -0500

    Make unit tests pass

--- database/factories/WebFactory.php
+++ database/factories/WebFactory.php
@@ -12,7 +12,6 @@
 $factory->define(App\Models\Web\Tag::class, function (Faker\Generator $faker) {
     return [
         'id' => $faker->unique()->randomNumber(4) + 999 * pow(10, 4),
-        'title' => ucfirst($faker->words(3, true)),
         'name' => ucfirst($faker->words(3, true)),
         'source_modified_at' => $faker->dateTimeThisYear,
         'created_at' => $faker->dateTimeThisYear,
