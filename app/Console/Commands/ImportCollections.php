5870b94e6165773b2fccaaa5db62c29d11173ebd
Illya Moskvin <imoskvin@artic.edu>
2018-03-29 20:33:58 -0500

    Consolidate ImportCollections and ImportCollectionsFull

--- app/Console/Commands/ImportCollections.php
+++ app/Console/Commands/ImportCollections.php
@@ -2,12 +2,11 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Collections\ArtworkType;
-
-class ImportCollections extends AbstractImportCommandNew
+class ImportCollections extends ImportCollectionsFull
 {
 
-    protected $signature = 'import:collections';
+    protected $signature = 'import:collections
+                            {endpoint? : Endpoint on dataservice to query, e.g. `object-types`}';
 
     protected $description = "Import collections data that has been updated since the last import";
 
@@ -20,38 +19,18 @@ class ImportCollections extends AbstractImportCommandNew
 
         $this->api = env('COLLECTIONS_DATA_SERVICE_URL');
 
-        $this->import( ArtworkType::class, 'object-types');
+        $endpoint = $this->argument('endpoint');
 
-        $this->importEndpoint('agent-types');
-        $this->importEndpoint('agent-places');
-        $this->importEndpoint('agents');
-        $this->importEndpoint('categories');
-        $this->importEndpoint('places');
-        $this->importEndpoint('galleries');
-        $this->importEndpoint('artwork-catalogues');
-        $this->importEndpoint('catalogues');
-        $this->importEndpoint('artworks');
-        $this->importEndpoint('links');
-        $this->importEndpoint('videos');
-        $this->importEndpoint('texts');
-        $this->importEndpoint('sounds');
-        $this->importEndpoint('images');
-        $this->importEndpoint('exhibition-agents');
-        $this->importEndpoint('exhibitions');
-        $this->importEndpoint('term-types');
-        $this->importEndpoint('terms');
+        if ($endpoint)
+        {
 
-    }
+            $this->importEndpoint($endpoint);
 
+        } else {
 
-    private function importEndpoint($endpoint, $page = 1)
-    {
+            $this->importEndpoints();
 
-        // TODO: This gets endpoints as outbound from our API
-        // Endpoints in the dataservice might be different!
-        $model = app('Resources')->getModelForEndpoint($endpoint);
-
-        $this->import( $model, $endpoint, $page );
+        }
 
     }
 
