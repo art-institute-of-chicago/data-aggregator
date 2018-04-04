ca22e495024e3ab4f04023cb8a32f066fe8daf0f
Illya Moskvin <imoskvin@artic.edu>
2018-04-04 16:33:19 -0500

    Refactor reset in ImportWebFull

--- app/Console/Commands/ImportWebFull.php
+++ app/Console/Commands/ImportWebFull.php
@@ -29,41 +29,10 @@ class ImportWebFull extends AbstractImportCommandNew
 
         $this->api = env('WEB_CMS_DATA_SERVICE_URL');
 
-        // Return false if the user bails out
-        if (!$this->option('yes') && !$this->confirm("Running this will delete all existing web cms data from your database! Are you sure?"))
+        if( !$this->reset() )
         {
             return false;
         }
-
-        // Remove all Publications and Sections from the search index
-        $this->call("scout:flush", ['model' => Article::class]);
-        $this->call("scout:flush", ['model' => Artist::class]);
-        $this->call("scout:flush", ['model' => Closure::class]);
-        $this->call("scout:flush", ['model' => Event::class]);
-        $this->call("scout:flush", ['model' => Exhibition::class]);
-        $this->call("scout:flush", ['model' => Hour::class]);
-        $this->call("scout:flush", ['model' => Location::class]);
-        //$this->call("scout:flush", ['model' => Page::class]);
-        $this->call("scout:flush", ['model' => Selection::class]);
-        $this->call("scout:flush", ['model' => Tag::class]);
-
-        // Truncate tables
-        DB::table('articles')->truncate();
-        DB::table('web_artists')->truncate();
-        DB::table('closures')->truncate();
-        DB::table('events')->truncate();
-        DB::table('web_exhibitions')->truncate();
-        DB::table('hours')->truncate();
-        DB::table('locations')->truncate();
-        //DB::table('pages')->truncate();
-        DB::table('selections')->truncate();
-        DB::table('tags')->truncate();
-
-        $this->info("Truncated web tables.");
-
-        // Flush might not remove models that are present in the index, but not the database
-        $this->info("Please manually ensure that your search index mappings are up-to-date. If there are models present "
-                    . "in the index but not the database, they were not flushed.");
 
         $this->import(Article::class, 'articles');
         $this->import(Artist::class, 'artists');
@@ -89,6 +58,38 @@ class ImportWebFull extends AbstractImportCommandNew
         }
 
         return parent::query( $endpoint, $page, $limit );
+    }
+
+    protected function reset()
+    {
+
+        return $this->resetData(
+            [
+                Article::class,
+                Artist::class,
+                Closure::class,
+                Event::class,
+                Exhibition::class,
+                Hour::class,
+                Location::class,
+                // Page::class,
+                Selection::class,
+                Tag::class,
+            ],
+            [
+                'articles',
+                'web_artists',
+                'closures',
+                'events',
+                'web_exhibitions',
+                'hours',
+                'locations',
+                // 'pages',
+                'selections',
+                'tags',
+            ]
+        );
+
     }
 
 }
