7fb76162a37ebbeb031acbc137cad08420d7743a
Illya Moskvin <imoskvin@artic.edu>
2018-04-12 21:05:21 -0500

    Fix migration for unit testing
    We really should consider moving off of SQLite to MySQL for testing.
    Still, if the speed sacrifice is too great, it might not be worth it.

--- database/migrations/2018_04_11_113943_fix_uids_in_artwork_category.php
+++ database/migrations/2018_04_11_113943_fix_uids_in_artwork_category.php
@@ -13,10 +13,18 @@ class FixUidsInArtworkCategory extends Migration
      */
     public function up()
     {
+
+        // TODO: SQLite doesn't have CONCAT()
+        if (App::environment('testing'))
+        {
+            return;
+        }
+
         DB::statement("UPDATE `artwork_category`"
             . "SET `category_lake_uid`=CONCAT('PC-', `category_lake_uid`)"
             . "WHERE `category_lake_uid` NOT LIKE 'PC-%'"
         );
+
     }
 
     /**
@@ -26,9 +34,11 @@ class FixUidsInArtworkCategory extends Migration
      */
     public function down()
     {
+
         DB::statement("UPDATE `artwork_category`"
             . " SET `category_lake_uid`=SUBSTR(`category_lake_uid`, 4)"
             . " WHERE `category_lake_uid` LIKE 'PC-%'"
         );
+
     }
 }
