875892a4b9e9833d14d8d2f33deb6be8321588f9
Illya Moskvin <imoskvin@artic.edu>
2018-04-30 17:02:38 -0500

    Fix override of ArtworkCatalogue's transform
    The model has an outbound structure that mimics the other pivot models,
    but this outbound transformer was overwriting that output.

--- app/Http/Transformers/ArtworkCatalogueTransformer.php
+++ app/Http/Transformers/ArtworkCatalogueTransformer.php
@@ -10,20 +10,4 @@ class ArtworkCatalogueTransformer extends CollectionsTransformer
     public $excludeDates = true;
     public $excludeIdsAndTitle = true;
 
-    /**
-     * Turn this item object into a generic array.
-     *
-     * @param  \App\Agent  $item
-     * @return array
-     */
-    public function transformFields($item)
-    {
-        return [
-            'is_preferred' => (bool) $item->preferred,
-            'catalogue' => $item->catalogue,
-            'number' => $item->number,
-            'state_edition' => $item->state_edition,
-        ];
-    }
-
 }
