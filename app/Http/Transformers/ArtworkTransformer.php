0542f672889fbb65e83b2378b07b33272ce1f237
Illya Moskvin <imoskvin@artic.edu>
2018-05-01 09:53:27 -0500

    Remove ArtworkCatalogueTransformer
    It's now functionally identical to PivotTransformer.

--- app/Http/Transformers/ArtworkTransformer.php
+++ app/Http/Transformers/ArtworkTransformer.php
@@ -115,7 +115,7 @@ class ArtworkTransformer extends CollectionsTransformer
      */
     public function includeCataloguePivots(Artwork $artwork)
     {
-        return $this->collection($artwork->artworkCatalogues, new ArtworkCatalogueTransformer, false);
+        return $this->collection($artwork->artworkCatalogues, new PivotTransformer, false);
     }
 
     /**
