aea4d0cbbba73741cece28eeb9d2c5e8dbdadefc
Illya Moskvin <imoskvin@artic.edu>
2018-05-10 16:46:49 -0500

    Add inbound transformer for Closure

---
+++ app/Transformers/Inbound/Web/Closure.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Transformers\Inbound\Web;
+
+use App\Transformers\Datum;
+use App\Transformers\Inbound\AbstractTransformer;
+
+class Closure extends AbstractTransformer
+{
+
+    use HasBlocks { getExtraFields as getBlockFields; }
+
+    protected function getTitle( Datum $datum )
+    {
+
+        // TODO: Make title nullable? Use date range as title?
+        // There's no title in the source system :/
+
+        return [
+            'title' => 'Lorem ipsum.',
+        ];
+
+    }
+
+    protected function getExtraFields( Datum $datum )
+    {
+
+        return [
+            'date_start' => $datum->date('date_start'),
+            'date_end' => $datum->date('date_end'),
+        ];
+
+    }
+
+}
