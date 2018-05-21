cf51465809c6c5c3544be83fee77155396138075
Illya Moskvin <imoskvin@artic.edu>
2018-05-21 11:25:02 -0500

    Move relationship helper methods to trait
    This is probably temporary. Just cleaning out the Artwork model to
    make it more manageable. I'd like this logic to be available to all
    models, so that we can cut down on code duplication and make things
    more standardized.

--- app/Models/Collections/Artwork.php
+++ app/Models/Collections/Artwork.php
@@ -5,6 +5,8 @@ namespace App\Models\Collections;
 use App\Models\CollectionsModel;
 use App\Models\ElasticSearchable;
 use App\Models\Documentable;
+use App\Models\HasRelationships;
+
 
 /**
  * Represents a work of art in our collections.
@@ -12,6 +14,7 @@ use App\Models\Documentable;
 class Artwork extends CollectionsModel
 {
 
+    use HasRelationships;
     use ElasticSearchable;
     use Documentable {
         docBoostedDescription as public traitDocBoostedDescription;
@@ -123,146 +126,6 @@ class Artwork extends CollectionsModel
     {
 
         return $this->hasMany('App\Models\Collections\ArtworkTerm');
-
-    }
-
-    /**
-     * Helper method to get the preferred term of a specified type from the eager-loaded array
-     * instead of executing a new SQL query.
-     */
-    public function preferred($resource = 'term', $type = '', $typeField = 'type')
-    {
-
-        $resources = $this->relatedResources($resource, $type, $typeField);
-
-        if (!$resources) return [];
-
-        $resources_ids = $this->relatedIds($resources);
-
-        $this->loadMissing($resource .'Pivots');
-
-        // Loop through all the term pivot models, only look at the ones
-        // of the specified type, and return the preferred one
-        foreach ($this->{$resource .'Pivots'} as $pivot)
-        {
-
-            $key = $pivot->{$resource}()->getForeignKey();
-            if (in_array($pivot->{$key}, $resources_ids))
-            {
-
-                if ($pivot->preferred)
-                {
-
-                    return head(array_where($resources, function ($value) use ($pivot, $key) {
-                        return $value->getKey() == $pivot->{$key};
-                    }));
-
-                }
-
-            }
-
-        }
-
-        return null;
-
-    }
-
-    /**
-     * Helper method to get the alternate term of a specified type from the eager-loaded array
-     * instead of executing a new SQL query.
-     */
-    public function alts($resource, $type = '', $typeField = 'type')
-    {
-
-        $resources = $this->relatedResources($resource, $type, $typeField);
-
-        if (!$resources) return [];
-
-        $resources_ids = $this->relatedIds($resources);
-
-        $this->loadMissing($resource .'Pivots');
-
-        // Loop through all the term pivot models, only look at the ones
-        // of the specified type, and return an array of the non-preferred ones
-        $ret = [];
-        foreach ($this->{$resource .'Pivots'} as $pivot)
-        {
-
-            $key = $pivot->{$resource}()->getForeignKey();
-            if (in_array($pivot->{$key}, $resources_ids))
-            {
-
-                if (!$pivot->preferred)
-                {
-
-                    $ret[] = head(array_where($resources, function ($value) use ($pivot, $key) {
-                        return $value->getKey() == $pivot->{$key};
-                    }));
-
-                }
-
-            }
-
-        }
-
-        return $ret;
-
-    }
-
-    /**
-     * Get all the resources to look through. If there is a subset of resource we're
-     * concerned with, as defined by $type, only get those. This is a helper method to
-     * get resources of a specified type from an eager-loaded array instead of executing
-     * a new SQL query.
-     */
-    private function relatedResources($resource, $type, $typeField = 'type')
-    {
-
-        // If no type is passed return an empty array
-        if (!$resource)
-        {
-
-            return [];
-
-        }
-
-        $this->loadMissing(str_plural($resource));
-
-        if (!$type)
-        {
-            return $this->{str_plural($resource)}->all();
-        }
-
-        // Loop through all the resources, and return just the ones of the specified type
-        $ret = [];
-        foreach ($this->{str_plural($resource)} as $res)
-        {
-
-            if ($res->{$typeField} == $type)
-            {
-
-                $ret[] = $res;
-
-            }
-
-        }
-
-        return $ret;
-
-    }
-
-    private function relatedIds($resources)
-    {
-
-        if ($resources)
-        {
-
-            $key = head($resources)->getKeyName();
-            return array_pluck($resources, $key);
-
-        }
-
-        return [];
 
     }
 
