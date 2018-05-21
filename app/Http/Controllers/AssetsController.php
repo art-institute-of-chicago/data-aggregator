a6396d44870c188035045932342580b267795218
Illya Moskvin <imoskvin@artic.edu>
2018-05-21 14:43:02 -0500

    Remove dead `respondInvalidSyntax` method

--- app/Http/Controllers/AssetsController.php
+++ app/Http/Controllers/AssetsController.php
@@ -13,10 +13,4 @@ class AssetsController extends BaseController
 
     protected $transformer = \App\Http\Transformers\AssetTransformer::class;
 
-
-    protected function respondInvalidSyntax($message = 'Invalid identifier', $detail = 'The id should be a universally unique identifier. Please ensure you\'re passing the correct source id and try again.')
-    {
-        return response()->error($message, $detail, Response::HTTP_BAD_REQUEST);
-    }
-
 }
