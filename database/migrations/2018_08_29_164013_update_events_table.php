6e7c7e38c3df908405e12f0cb039668140361776
Illya Moskvin <imoskvin@artic.edu>
2018-08-30 13:32:34 -0500

    Fix web events migration and factory for tests
    SQLite was complaining about multiple `dropColumn` declarations.
    I forgot to update the event factory to match our new schema.

--- database/migrations/2018_08_29_164013_update_events_table.php
+++ database/migrations/2018_08_29_164013_update_events_table.php
@@ -11,34 +11,21 @@ class UpdateEventsTable extends Migration
     {
 
         Schema::table('events', function (Blueprint $table) {
-            // $table->increments('id');
             $table->text('title')->change();
-            // $table->integer('type');
             $table->text('short_description')->change();
-            // $table->text('description')->nullable();
             $table->text('hero_caption')->change();
-            // $table->boolean('is_private');
-            // $table->boolean('is_after_hours');
-            // $table->boolean('is_ticketed');
-            // $table->boolean('is_free');
-            // $table->boolean('is_member_exclusive');
-            // $table->boolean('hidden');
             $table->text('rsvp_link')->change();
             $table->dateTime('start_date')->change();
             $table->dateTime('end_date')->change();
-            $table->dropColumn('all_dates')->change();
             $table->text('location')->change();
-            $table->dropColumn('sponsors_description')->change();
-            $table->dropColumn('sponsors_sub_copy')->change();
-            // $table->json('content')->nullable();
-            // $table->integer('layout_type');
             $table->text('buy_button_text')->change();
-            // $table->text('buy_button_caption')->nullable();
-            // $table->boolean('published');
-            // $table->timestamps();
-            // $table->softDeletes();
         });
 
+        foreach (['all_dates', 'sponsors_description', 'sponsors_sub_copy'] as $column) {
+            Schema::table('events', function (Blueprint $table) use ($column) {
+                $table->dropColumn($column);
+            });
+        }
     }
 
     /**
@@ -50,32 +37,17 @@ class UpdateEventsTable extends Migration
     {
 
         Schema::table('events', function (Blueprint $table) {
-            // $table->increments('id');
             $table->string('title')->change();
-            // $table->integer('type');
             $table->string('short_description')->change();
-            // $table->text('description')->nullable();
             $table->string('hero_caption')->change();
-            // $table->boolean('is_private');
-            // $table->boolean('is_after_hours');
-            // $table->boolean('is_ticketed');
-            // $table->boolean('is_free');
-            // $table->boolean('is_member_exclusive');
-            // $table->boolean('hidden');
             $table->string('rsvp_link')->change();
             $table->string('start_date')->change();
             $table->string('end_date')->change();
             $table->json('all_dates')->nullable()->after('end_date');
             $table->string('location')->change();
-            $table->text('sponsors_description')->nullable();
-            $table->text('sponsors_sub_copy')->nullable();
-            // $table->json('content')->nullable();
-            // $table->integer('layout_type');
+            $table->text('sponsors_description')->nullable()->after('location');
+            $table->text('sponsors_sub_copy')->nullable()->after('sponsors_sub_copy');
             $table->string('buy_button_text')->change();
-            // $table->text('buy_button_caption')->nullable();
-            // $table->boolean('published');
-            // $table->timestamps();
-            // $table->softDeletes();
         });
 
     }
