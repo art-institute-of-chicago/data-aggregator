7a44fe0840b7b9e1a5d06dc3d98af01c217c6367
Binyomin Greenes <bygreenes@gmail.com>
2018-09-17 13:37:16 -0400

    Adds a `match` method for eager loading
    I've found this class useful in my own projects. By adding the `match` method you will now get only a single result, not a collection with a single result when eager or lazy loading your relationship when you call `expectOne`.

--- app/BelongsToManyOrOne.php
+++ app/BelongsToManyOrOne.php
@@ -72,5 +72,23 @@ class BelongsToManyOrOne extends BelongsToMany
         return $this->wherePivot('preferred', '=', false)->expectMany();
 
     }
+    
+    public function match(array $models, Collection $results, $relation)
+    {
+        $dictionary = $this->buildDictionary($results);
+
+        // Once we have an array dictionary of child objects we can easily match the
+        // children back to their parent using the dictionary and the keys on the
+        // the parent models. Then we will return the hydrated models back out.
+        foreach ($models as $model) {
+            if (isset($dictionary[$key = $model->{$this->parentKey}])) {
+                $model->setRelation(
+                    $relation, $this->isMany ? $this->related->newCollection($dictionary[$key]) : $dictionary[$key]
+                );
+            }
+        }
+
+        return $models;
+    }
 
 }
