3397dfc9bb462dcbce43238e885012789347d05e
nikhiltri <ntrivedi@artic.edu>
2018-09-20 10:56:35 -0500

    Remove --quiet option from scheduled jobs, since we're not using it to supress emails anymore

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,13 +28,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 
-        $schedule->command('import:daily --quiet')
+        $schedule->command('import:daily')
             ->dailyAt('23:00')
             ->withoutOverlapping()
             ->appendOutputTo(storage_path('logs/import-daily.log'))
             ->sendOutputTo(storage_path('logs/import-daily-last-run.log'));
 
-        $schedule->command('import:monthly --quiet')
+        $schedule->command('import:monthly')
             ->monthlyOn(1, '03:00')
             ->withoutOverlapping()
             ->appendOutputTo(storage_path('logs/import-monthly.log'))
@@ -42,25 +42,25 @@ class Kernel extends ConsoleKernel
 
         // Because in the CMS Events don't get touched when a ticketed event
         // is added. Remove this once that's in place.
-        $schedule->command('import:web-full events --quiet')
+        $schedule->command('import:web-full events')
             ->hourly()
             ->withoutOverlapping()
             ->appendOutputTo(storage_path('logs/import-web-full.log'))
             ->sendOutputTo(storage_path('logs/import-web-full-last-run.log'));
 
-        $schedule->command('import:collections --quiet')
+        $schedule->command('import:collections')
             ->everyFiveMinutes()
             ->withoutOverlapping()
             ->appendOutputTo(storage_path('logs/import-collections.log'))
             ->sendOutputTo(storage_path('logs/import-collections-last-run.log'));
 
-        $schedule->command('import:web --quiet')
+        $schedule->command('import:web')
             ->everyFiveMinutes()
             ->withoutOverlapping()
             ->appendOutputTo(storage_path('logs/import-web.log'))
             ->sendOutputTo(storage_path('logs/import-web-last-run.log'));
 
-        $schedule->command('import:collections-delete --quiet')
+        $schedule->command('import:collections-delete')
             ->everyFiveMinutes()
             ->withoutOverlapping()
             ->appendOutputTo(storage_path('logs/import-collections-delete.log'))
