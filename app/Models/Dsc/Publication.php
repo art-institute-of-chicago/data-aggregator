c26c7de84a0b329574cbac5818a63bf4af9c33c5
nikhiltri <ntrivedi@artic.edu>
2018-12-12 17:06:18 -0600

    Provide link between DSC publication and page that represents it on the website [WEB-35]

--- app/Models/Dsc/Publication.php
+++ app/Models/Dsc/Publication.php
@@ -20,6 +20,42 @@ class Publication extends DscModel
 
     }
 
+    public function getGenericPageIdAttribute()
+    {
+        switch ($this->dsc_id) {
+        case 7:
+            return 9;
+            break;
+        case 12:
+            return 6;
+            break;
+        case 135446:
+            return 12;
+            break;
+        case 141096:
+            return 7;
+            break;
+        case 135466:
+            return 11;
+            break;
+        case 406:
+            return 13;
+            break;
+        case 445:
+            return 8;
+            break;
+        case 480:
+            return 5;
+            break;
+        case 226:
+            return 10;
+            break;
+        case 140019:
+            return 4;
+            break;
+        }
+    }
+
     /**
      * Specific field definitions for a given class. See `transformMapping()` for more info.
      */
