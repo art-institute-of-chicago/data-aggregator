53193acb5922bdc219dc44cd597077f9ca3273e3
Illya Moskvin <ivmoskvin@gmail.com>
2018-12-27 11:33:53 -0600

    Add dump:audit command

---
+++ app/Console/Commands/Dump/AbstractDumpCommand.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Console\Commands\Dump;
+
+use Aic\Hub\Foundation\AbstractCommand as BaseCommand;
+
+abstract class AbstractDumpCommand extends BaseCommand
+{
+
+    /**
+     * In the interest of defensive coding, you must explicitly whitelist
+     * which tables should be exported and imported. Use `dump:audit` to
+     * see a list of which tables will be excluded.
+     */
+    protected $whitelistedTables = [
+        'agent_roles',
+        'agent_site',
+        'agent_types',
+        'agents',
+        'archival_images',
+        'articles',
+        'artist_digital_label',
+        'artist_product',
+        'artwork_artist',
+        'artwork_artwork',
+        'artwork_asset',
+        'artwork_catalogue',
+        'artwork_category',
+        'artwork_date_qualifiers',
+        'artwork_dates',
+        'artwork_digital_label',
+        'artwork_exhibition',
+        'artwork_place',
+        'artwork_place_qualifiers',
+        'artwork_site',
+        'artwork_term',
+        'artwork_types',
+        'artworks',
+        'asset_category',
+        'assets',
+        'catalogues',
+        'category_place',
+        'category_terms',
+        'closures',
+        // 'commands',
+        'digital_catalogs',
+        'digital_label_exhibitions',
+        'digital_labels',
+        'educator_resources',
+        'event_occurrences',
+        'event_programs',
+        'events',
+        'exhibition_asset',
+        'exhibition_site',
+        'exhibitions',
+        // 'failed_jobs',
+        'galleries',
+        'generic_pages',
+        'hours',
+        // 'jobs',
+        'legacy_event_exhibition',
+        'legacy_events',
+        'library_material_creator',
+        'library_material_subject',
+        'library_materials',
+        'library_terms',
+        'locations',
+        // 'migrations',
+        'mobile_artwork_mobile_sound',
+        'mobile_artworks',
+        'mobile_sounds',
+        'places',
+        'press_releases',
+        'printed_catalogs',
+        'products',
+        'publications',
+        'research_guides',
+        'sections',
+        'selections',
+        'shop_categories',
+        'sites',
+        'tags',
+        // 'ticketed_event_types',
+        // 'ticketed_events',
+        'tour_stops',
+        'tours',
+        'web_artists',
+        'web_exhibitions',
+    ];
+
+}
---
+++ app/Console/Commands/Dump/DumpAudit.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands\Dump;
+
+use Illuminate\Support\Facades\DB;
+
+class DumpAudit extends AbstractDumpCommand
+{
+
+    protected $signature = 'dump:audit';
+
+    protected $description = "Shows which tables will be excluded from the dump";
+
+
+    public function handle()
+    {
+
+        $allTables = DB::connection()->getDoctrineSchemaManager()->listTableNames();
+        $excludedTables = array_diff($allTables, $this->whitelistedTables);
+
+        $this->warn('Tables excluded from the data dump:');
+
+        array_walk($excludedTables, function($item, $key) {
+            $this->info($item);
+        });
+
+    }
+
+}
