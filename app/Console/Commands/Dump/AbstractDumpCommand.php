64cc882b66f4bf518a4f21835d758731aee40415
Illya Moskvin <ivmoskvin@gmail.com>
2018-12-29 02:51:19 -0600

    Lift methods from dump:upload to parent

--- app/Console/Commands/Dump/AbstractDumpCommand.php
+++ app/Console/Commands/Dump/AbstractDumpCommand.php
@@ -105,4 +105,70 @@ abstract class AbstractDumpCommand extends BaseCommand
 
     }
 
+    /**
+     * Use this when you need to run a command interactively or show ouput.
+     */
+    protected function passthru(string $template, string ...$args)
+    {
+        return $this->command($template, $args, function(string $cmd) {
+            passthru($cmd, $status);
+            return [
+                'output' => null,
+                'status' => $status,
+            ];
+        });
+    }
+
+    /**
+     * Use this when you need to capture command output in a variable.
+     */
+    protected function exec(string $template, string ...$args) : array
+    {
+        return $this->command($template, $args, function(string $cmd) {
+            exec($cmd, $output, $status);
+            return [
+                'output' => $output,
+                'status' => $status,
+            ];
+        });
+    }
+
+    /**
+     * Sanitize and run shell command. Exit if it fails. Return output or null.
+     */
+    protected function command(string $template, array $args, callable $callback)
+    {
+        $args = array_map('escapeshellarg', $args);
+        $cmd = vsprintf($template, $args);
+
+        $this->warn($cmd);
+
+        $return = $callback($cmd);
+
+        $this->warn('Status: ' . $return['status']);
+
+        if ($return['status'] !== 0)
+        {
+            $this->warn('Something went wrong. Exiting early.');
+            exit(1);
+        }
+
+        return $return['output'];
+    }
+
+    /**
+     * Throw an exception if an `.env` var is empty.
+     */
+    protected function validateEnv(array $vars)
+    {
+        foreach ($vars as $var)
+        {
+            if (empty(env($var)))
+            {
+                throw new Exception('Please specify `' . $var . '` in .env');
+            }
+        }
+
+    }
+
 }
--- app/Console/Commands/Dump/DumpUpload.php
+++ app/Console/Commands/Dump/DumpUpload.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands\Dump;
 
-use Illuminate\Support\Facades\DB;
 use Exception;
 
 class DumpUpload extends AbstractDumpCommand
@@ -79,72 +78,6 @@ class DumpUpload extends AbstractDumpCommand
         );
 
         $this->passthru('git -C %s push %s', $repoPath, ($this->option('reset') ? '--force' : ''));
-
-    }
-
-    /**
-     * Use this when you need to run a command interactively or show ouput.
-     */
-    private function passthru(string $template, string ...$args)
-    {
-        return $this->command($template, $args, function(string $cmd) {
-            passthru($cmd, $status);
-            return [
-                'output' => null,
-                'status' => $status,
-            ];
-        });
-    }
-
-    /**
-     * Use this when you need to capture command output in a variable.
-     */
-    private function exec(string $template, string ...$args) : array
-    {
-        return $this->command($template, $args, function(string $cmd) {
-            exec($cmd, $output, $status);
-            return [
-                'output' => $output,
-                'status' => $status,
-            ];
-        });
-    }
-
-    /**
-     * Sanitize and run shell command. Exit if it fails. Return output or null.
-     */
-    private function command(string $template, array $args, callable $callback)
-    {
-        $args = array_map('escapeshellarg', $args);
-        $cmd = vsprintf($template, $args);
-
-        $this->warn($cmd);
-
-        $return = $callback($cmd);
-
-        $this->warn('Status: ' . $return['status']);
-
-        if ($return['status'] !== 0)
-        {
-            $this->warn('Something went wrong. Exiting early.');
-            exit(1);
-        }
-
-        return $return['output'];
-    }
-
-    /**
-     * Throw an exception if an `.env` var is empty.
-     */
-    private function validateEnv(array $vars)
-    {
-        foreach ($vars as $var)
-        {
-            if (empty(env($var)))
-            {
-                throw new Exception('Please specify `' . $var . '` in .env');
-            }
-        }
 
     }
 
