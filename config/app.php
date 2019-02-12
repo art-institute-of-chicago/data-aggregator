9a9653dffaf84b17be4cfa78b717d494dd2444d8
Illya Moskvin <imoskvin@artic.edu>
2019-02-12 14:43:32 -0600

    Bump version number

--- config/app.php
+++ config/app.php
@@ -21,7 +21,7 @@ return [
     | the framework needs to place the application's version in a notification
     | or any other location as required by the application or its packages.
     */
-    'version' => '1.0-beta14',
+    'version' => '1.0-beta15',
 
 
     /*
