b69c529f3d16235b207cf02ea8a22b39e09caa57
nikhiltri <ntrivedi@artic.edu>
2019-03-05 13:15:04 -0600

    Update header for proxy midleware [WEB-889]

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -19,5 +19,5 @@ class TrustProxies extends Middleware
      *
      * @var string
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
 }
