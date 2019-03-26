823d0c34e327a417ca01f32188c9d7128e94e55a
nikhiltri <ntrivedi@artic.edu>
2019-03-26 15:40:19 -0500

    First pass at getting authentication set up [WEB-133]
    I'm currently stuck on the Frontend Quickstart section of the Passport
    documentation. We've never used Laravel's built-in frontend before, so
    much of the work in this commit has been re-engineering that to get it
    to function at a basic level. This commit represents all the basic
    pieces in place, with added Vue components for Passport, but the compiled
    frontend doesn't seem to recognize the new component tags.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<example-component></example-component>
+hello
+<passport-clients></passport-clients>
+yea
+<passport-authorized-clients></passport-authorized-clients>
+saturday
+<passport-personal-access-tokens></passport-personal-access-tokens>
+bye
