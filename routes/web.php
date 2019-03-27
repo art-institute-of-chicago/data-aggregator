9e990acb160f745caebd035cfe39f0450527d99a
nikhiltri <ntrivedi@artic.edu>
2019-03-27 11:29:36 -0500

    Get web authentication in place [WEB-133]

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::any('/', function () {
-    return redirect('api');
+    return redirect('home');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->get('/user', function () {
+    return Auth::user();
 });
