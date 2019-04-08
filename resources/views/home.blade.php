37b95f56dc9a056aa43376cadfe534f04b7b0488
nikhiltri <ntrivedi@artic.edu>
2019-04-08 15:57:38 -0500

    Treat /home as the web homepage [WEB-133]

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,6 +18,19 @@
                 </div>
             </div>
         </div>
+
+        <div class="col-md-8">
+            <passport-clients></passport-clients>
+        </div>
+
+        <div class="col-md-8">
+            <passport-authorized-clients></passport-authorized-clients>
+        </div>
+
+        <div class="col-md-8">
+            <passport-personal-access-tokens></passport-personal-access-tokens>
+        </div>
+
     </div>
 </div>
 @endsection
