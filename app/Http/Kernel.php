73ba15bc153b448adb87cdbd5d233a5cd9988d3d
nikhiltri <ntrivedi@artic.edu>
2019-04-09 10:04:52 -0500

    Add middleware to allow JS frontends to query API with tokens [WEB-133]

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
         ],
 
         'api' => [
