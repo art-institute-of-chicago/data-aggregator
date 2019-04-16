a2349a922d64072b0e239d20b17333e758fe30d2
Illya Moskvin <ivmoskvin@gmail.com>
2019-04-15 23:15:14 -0500

    Fix incorrect datetime value error on TicketedEvent

--- app/Models/Membership/TicketedEvent.php
+++ app/Models/Membership/TicketedEvent.php
@@ -27,6 +27,7 @@ class TicketedEvent extends MembershipModel
         'is_after_hours' => 'boolean',
         'is_private_event' => 'boolean',
         'is_admission_required' => 'boolean',
+        'source_created_at' => 'datetime',
     ];
 
     public function event()
