b978ba8083e00fb088ae2ad0f51a64c51eb73d5e
Illya Moskvin <imoskvin@artic.edu>
2019-04-19 17:06:47 -0500

    Squash `add_agent_exhibition_fields`

--- database/migrations/2017_06_02_052230_create_collections_tables.php
+++ database/migrations/2017_06_02_052230_create_collections_tables.php
@@ -210,6 +210,18 @@ class CreateCollectionsTables extends Migration
             $table->increments('id');
             $table->integer('agent_citi_id')->unsigned()->index();
             $table->integer('exhibition_citi_id')->unsigned()->index();
+            $table->date('date_start')->nullable();
+            $table->date('date_end')->nullable();
+            $table->boolean('is_host')->nullable();
+            $table->boolean('is_organizer')->nullable();
+            $table->integer('agent_citi_id')->unsigned()->nullable()->change();
+            $table->integer('exhibition_citi_id')->unsigned()->nullable()->change();
+            $table->timestamp('source_created_at')->nullable();
+            $table->timestamp('source_modified_at')->nullable();
+            $table->timestamp('source_indexed_at')->nullable();
+            $table->timestamp('citi_created_at')->nullable();
+            $table->timestamp('citi_modified_at')->nullable();
+            $table->timestamps();
         });
 
 
