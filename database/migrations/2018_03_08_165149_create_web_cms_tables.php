552a1b255be5af2d8e201453fb4d643369d5f6f2
Illya Moskvin <ivmoskvin@gmail.com>
2019-04-22 14:49:51 -0500

    Squash `create_event_occurrences_table`

--- database/migrations/2018_03_08_165149_create_web_cms_tables.php
+++ database/migrations/2018_03_08_165149_create_web_cms_tables.php
@@ -106,6 +106,26 @@ class CreateWebCmsTables extends Migration
             $table->softDeletes();
         });
 
+        Schema::create('event_occurrences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title')->nullable();
+            $table->integer('event_id')->nullable();
+            $table->text('short_description')->nullable();
+            $table->text('description')->nullable();
+            $table->text('image_url')->nullable();
+            $table->text('image_caption')->nullable();
+            $table->boolean('is_private')->nullable();
+            $table->text('location')->nullable();
+            $table->timestamp('start_at')->nullable();
+            $table->timestamp('end_at')->nullable();
+            $table->text('button_text')->nullable();
+            $table->text('button_url')->nullable();
+            $table->text('button_caption')->nullable();
+            $table->timestamp('source_created_at')->nullable();
+            $table->timestamp('source_modified_at')->nullable();
+            $table->timestamps();
+        });
+
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
@@ -176,6 +196,7 @@ class CreateWebCmsTables extends Migration
         Schema::dropIfExists('closures');
         Schema::dropIfExists('web_exhibitions');
         Schema::dropIfExists('events');
+        Schema::dropIfExists('event_occurrences');
         Schema::dropIfExists('articles');
         Schema::dropIfExists('selections');
         Schema::dropIfExists('web_artists');
