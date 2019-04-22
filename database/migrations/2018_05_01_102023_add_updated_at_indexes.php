37b994da045c08a16334c98e699753e8f2b9c0dc
Illya Moskvin <ivmoskvin@gmail.com>
2019-04-22 14:16:07 -0500

    Condense `add_updated_at_indexes`

---
+++ database/migrations/2018_05_01_102023_add_updated_at_indexes.php
@@ -0,0 +1,83 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUpdatedAtIndexes extends Migration
+{
+    /**
+     * TODO: Create custom `timestamps()` function?
+     */
+    private $tableNames = [
+        'agent_exhibition',
+        'agent_place',
+        'agent_roles',
+        'agent_types',
+        'agents',
+        'archival_images',
+        'articles',
+        'artwork_artwork',
+        'artwork_catalogue',
+        'artwork_date_qualifiers',
+        'artwork_dates',
+        'artwork_place_qualifiers',
+        'artwork_term',
+        'artwork_types',
+        'artworks',
+        'assets',
+        'catalogues',
+        'category_terms',
+        'closures',
+        'commands',
+        'events',
+        'exhibitions',
+        'galleries',
+        'hours',
+        'legacy_events',
+        'library_material_creator',
+        'library_material_subject',
+        'library_materials',
+        'library_terms',
+        'locations',
+        'mobile_artworks',
+        'mobile_sounds',
+        'pages',
+        'places',
+        'products',
+        'publications',
+        'sections',
+        'selections',
+        'shop_categories',
+        'sites',
+        'tags',
+        'ticketed_events',
+        'tour_stops',
+        'tours',
+        'web_artists',
+        'web_exhibitions',
+    ];
+
+    public function up()
+    {
+        // TODO: Creating indexes isn't friendly with SQLite
+        if (App::environment('testing')) {
+            return;
+        }
+
+        foreach ($this->tableNames as $tableName) {
+            Schema::table($tableName, function(Blueprint $table) {
+                $table->index('updated_at');
+            });
+        }
+    }
+
+    public function down()
+    {
+        foreach ($this->tableNames as $tableName) {
+            Schema::table($tableName, function(Blueprint $table) {
+                $table->dropIndex(['updated_at']);
+            });
+        }
+    }
+}
