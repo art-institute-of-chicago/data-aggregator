08d325b51947a14ba6a2e4e70b51452193d442c3
Illya Moskvin <imoskvin@artic.edu>
2019-04-24 16:41:52 -0500

    Refactor `import:mobile` command to use `appData-v2.json`
    Not much has changed here. Using `tour_stops` and `audio_commentary`.
    Focus is being shifted from tour stops to mobile sounds.

--- app/Console/Commands/Import/ImportMobile.php
+++ app/Console/Commands/Import/ImportMobile.php
@@ -17,7 +17,7 @@ class ImportMobile extends AbstractImportCommand
 {
     use ConvertsToHtml;
 
-    protected $signature = 'import:mobile';
+    protected $signature = 'import:mobile {--no-download}';
 
     protected $description = "Import all data from the mobile CMS";
 
@@ -26,215 +26,119 @@ class ImportMobile extends AbstractImportCommand
 
     public function handle()
     {
+        if (!$this->option('no-download')) {
+            $this->info('Retrieving events JSON from artic.edu');
+            $contents = $this->fetch(env('MOBILE_JSON'));
+            Storage::disk('local')->put($this->filename, $contents);
+        }
 
-        $this->info('Retrieving events JSON from artic.edu');
-
-        $contents = $this->fetch( env('MOBILE_JSON') );
-
-        Storage::disk('local')->put($this->filename, $contents);
-
-        // Spoofing this w/ local file for speed
         $contents = Storage::get($this->filename);
+        $results = json_decode($contents);
 
-        $results = json_decode( $contents );
-
-        // There's no unique data coming re: galleries from the mobile app AFAICT
-
-        $this->importArtworks( $results );
-        $this->importSounds( $results );
-        $this->importTours( $results );
-        // TourStops are imported inside importTours()
-
+        $this->importArtworks($results);
+        $this->importSounds($results);
+        $this->importTours($results);
     }
-
 
     private function importArtworks( $results )
     {
-
         $this->info("Importing mobile artworks...");
 
-        foreach( $results->objects as $datum )
-        {
-
+        foreach ($results->objects as $datum) {
             $this->info("Importing artwork #{$datum->nid}: {$datum->title}");
 
-            // Ex: 41.8794289180879, -87.6236425536961
-            $location = explode(', ', $datum->location);
+            $id = (int) $datum->nid;
+            $artwork = MobileArtwork::findOrNew($id);
 
-            $artwork = MobileArtwork::findOrNew( (int) $datum->nid );
-
-            $artwork->mobile_id = (int) $datum->nid;
+            $artwork->mobile_id = $id;
             $artwork->title = $datum->title;
 
-            // $artwork->artwork()->attach( $base );
-            // $artwork->artwork()->associate( $base );
             $artwork->artwork_citi_id = $datum->object_id ?? null;
 
-            // Pull in an actual model
-            // $artwork->artwork_citi_id = (int) $datum->object_id,
-
-            // In some rare cases the mobile app returns this value as an array of obejcts instead of an array of
-            // simple integers. We'll account for both:
-            if ($datum->audio == array_filter($datum->audio, 'is_numeric'))
-            {
-
-                // https://stackoverflow.com/questions/17472128/preventing-laravel-adding-multiple-records-to-a-pivot-table
-                $artwork->sounds()->sync( $datum->audio, false );
-
-            }
-            else
-            {
-
-                $artwork->sounds()->sync( collect($datum->audio)->pluck('audio')->all(), false );
-
-            }
-
+            $location = explode(', ', $datum->location);
             $artwork->latitude = (float) $location[0];
             $artwork->longitude = (float) $location[1];
 
+            $artwork->sounds()->sync(collect($datum->audio_commentary)->pluck('audio')->all());
+
             $artwork->save();
-
         }
-
     }
-
 
     private function importSounds( $results )
     {
-
         $this->info("Importing mobile sounds...");
 
-        foreach( $results->audio_files as $datum )
-        {
+        Sound::query()->delete();
 
+        foreach ($results->audio_files as $datum) {
             $this->info("Importing audio #{$datum->nid}: {$datum->title}");
 
-            $sound = Sound::findOrNew( (int) $datum->nid );
+            $id = (int) $datum->nid;
+            $sound = Sound::findOrNew($id);
 
-            $sound->mobile_id = (int) $datum->nid;
+            $sound->mobile_id = $id;
             $sound->title = $datum->title;
 
             $sound->link = env('MOBILE_AUDIO_CDN_URL') . array_slice(explode('/', $datum->audio_file_url), -1)[0];
             $sound->transcript = $this->convertToHtml($datum->audio_transcript);
 
-            $sound->save();
-
             // Sounds are attached to Artworks on the Artwork side
 
+            $sound->save();
         }
-
     }
-
 
     private function importTours( $results )
     {
-
         $this->info("Importing mobile tours and tour stops...");
 
-        foreach( $results->tours as $datum )
-        {
+        Tour::query()->delete();
 
+        foreach ($results->tours as $datum) {
             $this->info("Importing tour #{$datum->nid}: {$datum->title}");
 
-            $tour = Tour::findOrNew( (int) $datum->nid );
+            $id = (int) $datum->nid;
+            $tour = Tour::findOrNew($id);
 
-            $tour->mobile_id = (int) $datum->nid;
+            $tour->mobile_id = $id;
             $tour->title = $datum->title;
 
             $tour->image = $datum->image_url;
             $tour->intro_text = $this->convertToHtml($datum->intro);
             $tour->description = $this->convertToHtml($datum->description);
 
-            $tour->intro()->associate( $datum->tour_audio );
+            $tour->intro()->associate($datum->tour_audio);
 
             $tour->save();
 
-            $this->importTourStops( $datum->stops, $tour );
-
+            $this->importTourStops($datum->tour_stops, $tour);
         }
-
     }
-
 
     private function importTourStops( $data, $tour )
     {
-
         $this->info("Flushing tour stops for tour #{$tour->mobile_id}...");
 
         TourStop::where('tour_mobile_id', $tour->mobile_id)->delete();
 
         $this->info("Importing tour stops for tour #{$tour->mobile_id}...");
 
-        foreach( $data as $datum )
-        {
+        foreach ($data as $datum) {
+            $this->info("Importing tour stop [ {$tour->mobile_id} / {$datum->object} / {$datum->audio_id} ]");
 
-            $this->info("Importing tour stop [ {$tour->mobile_id} / {$datum->object} / {$datum->audio} ]");
+            $id = cantorTuple($datum->object, $datum->audio_id);
 
-            $id = cantorTuple( $datum->object, $datum->audio );
-
-            $stop = TourStop::findOrNew( $id );
+            $stop = TourStop::findOrNew($id);
 
             $stop->id = $id;
             $stop->weight = $datum->sort;
 
-            $stop->tour()->associate( $tour->mobile_id );
-            $stop->artwork()->associate( $datum->object );
-            $stop->sound()->associate( $datum->audio );
+            $stop->tour()->associate($tour->mobile_id);
+            $stop->artwork()->associate($datum->object);
+            $stop->sound()->associate($datum->audio_id);
 
             $stop->save();
-
         }
-
     }
-
-
-    // They are.
-    private function checkIfObjectsAndAudioFilesAreM2M( $results )
-    {
-
-        $audios = collect( $results->objects )->pluck('audio');
-
-        $combined = collect([]);
-
-        foreach( $audios as $audio ) {
-            $combined = $combined->merge( $audio );
-        }
-
-        $counts = array_count_values( $combined->toArray() );
-
-        $files = collect( $results->audio_files );
-
-        $one = [];
-        $none = [];
-        $many = [];
-
-        foreach( $files as $file ) {
-
-            if( !isset( $counts[ $file->nid ] ) ) {
-                array_push( $none, $file->nid );
-                continue;
-            }
-
-            switch( $counts[ $file->nid ] ) {
-                case 1:
-                    array_push( $one, $file->nid );
-                break;
-                default:
-                    array_push( $many, $file->nid );
-                break;
-            }
-
-        }
-
-        $results = [
-            'many' => $many,
-            'none' => $none,
-            'one' => $one,
-        ];
-
-
-        dd( $results );
-
-    }
-
 }
