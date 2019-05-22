6891d80b366db2909f9e750b0d2673a195572a69
nikhiltri <ntrivedi@artic.edu>
2019-05-22 17:05:58 -0500

    Pass all requests through authorization [WEB-133]
    This change makes sure all requests gets passed through an authentication
    mechanism. Anonymous requests will still provided data, but we'll be able
    to have them behave differently by checking Auth::check().

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,10 +2,43 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+
+    /**
+     * Determine if the user is logged in to any of the given guards.
+     * If they're not logged in, still let them through. The app will limit
+     * what they can see and do.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function authenticate($request, array $guards)
+    {
+        if (empty($guards)) {
+            $guards = [null];
+        }
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                return $this->auth->shouldUse($guard);
+            }
+            else {
+                return null;
+            }
+        }
+
+        throw new AuthenticationException(
+            'Unauthenticated.', $guards, $this->redirectTo($request)
+        );
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
