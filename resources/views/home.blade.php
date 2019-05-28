b2d739ccec00dbf888d041e6e4e74888f01c404c
nikhiltri <ntrivedi@artic.edu>
2019-05-28 13:59:14 -0500

    Clean up web UI [WEB-133]

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,25 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        @if (session('status'))
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
-                    @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
-
-                    You are logged in!
                 </div>
             </div>
         </div>
-
-        <div class="col-md-8">
-            <passport-clients></passport-clients>
-        </div>
+        @endif
 
         <div class="col-md-8">
             <passport-personal-access-tokens></passport-personal-access-tokens>
