7e95610001fe99591f00f280ccc2de19de874f2d
nikhiltri <ntrivedi@artic.edu>
2019-09-11 12:31:32 -0500

    Rely on built-in Auth::routes function instead of replicating it [WEB-1203]

--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,7 @@ Route::any('/', function () {
 });
 
 Route::group(['middleware' => ['checkIp']], function() {
-
-    Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
-    Route::post('/login', 'Auth\LoginController@login');
-    Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
-
-    // Registration Routes...
-    Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-    Route::post('/register', 'Auth\RegisterController@register');
-
-    // Password Reset Routes...
-    Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-    Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-    Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-    Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
-
+    Auth::routes();
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
