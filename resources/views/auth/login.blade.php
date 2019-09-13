455bc6948e3f5cd331a2721f48fdc407f1298645
nikhiltri <ntrivedi@artic.edu>
2019-09-13 12:25:45 -0500

    Style authentication pages [WEB-1114]

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        <header class="m-article-header">
+            <div class="m-article-header__text">
+                <h1 class="title f-headline" itemprop="name">{{ __('Login') }}</h1>
             </div>
+        </header>
+
+        <div class="o-article__primary-actions"></div>
+        <div class="o-article__secondary-actions"></div>
+
+        <div class="o-article__body o-blocks">
+            <form class="o-form o-blocks o-blocks__block" method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <fieldset class="m-fieldset">
+                    <ol class="m-fieldset__fields">
+                        <li class="m-fieldset__field o-blocks">
+                            <span class="input s-disabled">
+                                <label for="email" class="label f-secondary">{{ __('E-Mail Address') }}</label>
+                                <input class="f-secondary {{ $errors->has('email') ? ' is-invalid' : '' }}" type="email" value="{{ old('email') }}" id="email" name="email" placeholder="" required autofocus>
+                                @if ($errors->has('email'))
+                                    <em class="error-msg f-secondary">{{ $errors->first('email') }}</em>
+                                @endif
+                            </span>
+                        </li>
+
+                        <li class="m-fieldset__field o-blocks">
+                            <span class="input s-disabled">
+                                <label for="password" class="label f-secondary">{{ __('Password') }}</label>
+                                <input class="f-secondary {{ $errors->has('password') ? ' is-invalid' : '' }}" type="password" value="{{ old('password') }}" id="password" name="password" placeholder="" required>
+                                @if ($errors->has('email'))
+                                    <em class="error-msg f-secondary">{{ $errors->first('password') }}</em>
+                                @endif
+                            </span>
+                        </li>
+
+                        <li class="m-fieldset__field o-blocks m-fieldset__field--group">
+                            <span class="checkbox s-disabled">
+                                <input type="checkbox" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <span class="f-secondary">
+                                    <label for="remember" class="label  ">{{ __('Remember Me') }}</label>
+                                </span>
+                            </span>
+                        </li>
+                    </ol>
+                </fieldset>
+
+                <ul class="o-form__actions">
+                    <li class="o-form__action">
+                        <button class="btn f-buttons" type="submit">{{ __('Login') }}</button>
+                    </li>
+                    @if (Route::has('password.request'))
+                        <li class="o-form__action">
+                            <a class="btn btn--secondary f-buttons" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        </li>
+                    @endif
+                </ul>
+            </form>
         </div>
-    </div>
-</div>
 @endsection
