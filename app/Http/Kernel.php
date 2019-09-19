b6090e900b3bc6f1f19046cca6622ef67c46b856
nikhiltri <ntrivedi@artic.edu>
2019-09-19 15:26:29 -0500

    Remove unneeded override of VerifyCsrfToken
    Our "fix" to remove CSRF token verification only worked because we
    were removing the requirement to execute this verification on all posts.
    It appears the problem was due to the forms being cached, and passing
    stale tokens to the controller. We cleaned up the CDN to not cache the
    forms, so CSRF verification now works.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
         ],
