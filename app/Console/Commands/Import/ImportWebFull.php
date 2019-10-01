cfd443a5414bb265f21b2f91c391ac38b32f8dc8
Illya Moskvin <imoskvin@artic.edu>
2019-10-01 15:11:44 -0500

    Reduce page size for web imports

--- app/Console/Commands/Import/ImportWebFull.php
+++ app/Console/Commands/Import/ImportWebFull.php
@@ -132,7 +132,7 @@ class ImportWebFull extends AbstractImportCommand
         return $this->import('Web', $model, $endpoint, $page);
     }
 
-    protected function query($endpoint, $page = 1, $limit = 100)
+    protected function query($endpoint, $page = 1, $limit = 10)
     {
         if (env('WEB_CMS_DATA_SERVICE_USERNAME')) {
             $this->auth = env('WEB_CMS_DATA_SERVICE_USERNAME') . ':' . env('WEB_CMS_DATA_SERVICE_PASSWORD');
