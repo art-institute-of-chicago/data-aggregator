e5ea7af9ff37dde8807bb2e77b16d36edbf5cd9a
Illya Moskvin <imoskvin@artic.edu>
2019-10-01 13:31:56 -0500

    Add --since option to import:web

--- app/Console/Commands/Import/ImportWeb.php
+++ app/Console/Commands/Import/ImportWeb.php
@@ -5,7 +5,8 @@ namespace App\Console\Commands\Import;
 class ImportWeb extends ImportWebFull
 {
 
-    protected $signature = 'import:web';
+    protected $signature = 'import:web
+                            {--since= : How far back to scan for records}';
 
     protected $description = 'Import web CMS data that has been updated since the last import';
 
