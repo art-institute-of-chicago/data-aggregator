29613b09a1b40b2b2cf55e3bcffbf523a67b0d16
Illya Moskvin <imoskvin@artic.edu>
2019-11-06 14:02:26 -0600

    Lint indentation in views [WEB-1114]

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -1,29 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-        @if (session('status'))
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+
+@if (session('status'))
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-body">
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
             </div>
         </div>
-        @endif
+    </div>
+@endif
 
-        <header class="m-article-header">
-            <div class="m-article-header__text">
-                <h1 class="title f-headline" itemprop="name">{{ $title }}</h1>
-            </div>
-        </header>
+<header class="m-article-header">
+    <div class="m-article-header__text">
+        <h1 class="title f-headline" itemprop="name">{{ $title }}</h1>
+    </div>
+</header>
 
-        <div class="o-article__primary-actions"></div>
-        <div class="o-article__secondary-actions"></div>
+<div class="o-article__primary-actions"></div>
+<div class="o-article__secondary-actions"></div>
 
-        <div class="o-article__body o-blocks">
-            {!! $content !!}
-        </div>
+<div class="o-article__body o-blocks">
+    {!! $content !!}
+</div>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,70 +19,67 @@
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/612324/7579192/css/fonts.css">
     <style>
-      @import url("//hello.myfonts.net/count/3545d5");
-        @font-face {font-family: 'Sabon';src: url(/assets/fonts/3545D5_0_0.eot);src: url(/assets/fonts/3545D5_0_0.eot?#iefix) format('embedded-opentype'),url(/assets/fonts/3545D5_0_0.woff2) format('woff2'),url(/assets/fonts/3545D5_0_0.woff) format('woff'),url(/assets/fonts/3545D5_0_0.ttf) format('truetype');font-weight:normal;font-weight:400;font-style:normal;}
-        @font-face {font-family: 'Sabon';src: url(/dist/fonts/3545D5_1_0.eot);src: url(/dist/fonts/3545D5_1_0.eot?#iefix) format('embedded-opentype'),url(/dist/fonts/3545D5_1_0.woff2) format('woff2'),url(/dist/fonts/3545D5_1_0.woff) format('woff'),url(/dist/fonts/3545D5_1_0.ttf) format('truetype');font-weight:normal;font-weight:400;font-style:italic;}
-        @font-face {font-family: 'Sabon';src: url(/dist/fonts/3545D5_2_0.eot);src: url(/dist/fonts/3545D5_2_0.eot?#iefix) format('embedded-opentype'),url(/dist/fonts/3545D5_2_0.woff2) format('woff2'),url(/dist/fonts/3545D5_2_0.woff) format('woff'),url(/dist/fonts/3545D5_2_0.ttf) format('truetype');font-weight:normal;font-weight:500;font-style:normal;}
+    @import url("//hello.myfonts.net/count/3545d5");
+    @font-face {font-family: 'Sabon';src: url(/assets/fonts/3545D5_0_0.eot);src: url(/assets/fonts/3545D5_0_0.eot?#iefix) format('embedded-opentype'),url(/assets/fonts/3545D5_0_0.woff2) format('woff2'),url(/assets/fonts/3545D5_0_0.woff) format('woff'),url(/assets/fonts/3545D5_0_0.ttf) format('truetype');font-weight:normal;font-weight:400;font-style:normal;}
+    @font-face {font-family: 'Sabon';src: url(/dist/fonts/3545D5_1_0.eot);src: url(/dist/fonts/3545D5_1_0.eot?#iefix) format('embedded-opentype'),url(/dist/fonts/3545D5_1_0.woff2) format('woff2'),url(/dist/fonts/3545D5_1_0.woff) format('woff'),url(/dist/fonts/3545D5_1_0.ttf) format('truetype');font-weight:normal;font-weight:400;font-style:italic;}
+    @font-face {font-family: 'Sabon';src: url(/dist/fonts/3545D5_2_0.eot);src: url(/dist/fonts/3545D5_2_0.eot?#iefix) format('embedded-opentype'),url(/dist/fonts/3545D5_2_0.woff2) format('woff2'),url(/dist/fonts/3545D5_2_0.woff) format('woff'),url(/dist/fonts/3545D5_2_0.ttf) format('truetype');font-weight:normal;font-weight:500;font-style:normal;}
     </style>
     <link href="/assets/styles/app.css" rel="stylesheet">
 </head>
 <body>
     <div id="a17">
-      <header class="g-header">
-        <div class="g-header__inner">
-            <a href="#content" class="skip-nav f-body">Skip to Content</a>
-            <nav aria-label="primary">
-                      <a class="g-header__logo" aria-label="Art Institute of Chicago" href="/">
-                <svg aria-hidden="true">
-                  <use xlink:href="#icon--logo--80"></use>
-                  <use xlink:href="#icon--logo--88"></use>
-                  <use xlink:href="#icon--logo--92"></use>
-                </svg>
-              </a>
-              <div class="g-header__nav-primary">
-                <h2 class="sr-only" id="h-nav-primary-header">Primary Navigation</h2>
-                <ul class="f-main-nav" aria-labelledby="h-nav-primary-header">
-                        <li>
-                            <a href="{{ route('doc-endpoints') }}">{{ __('Endpoints') }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('doc-fields') }}">{{ __('Fields') }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('doc-swagger') }}">{{ __('swagger.json') }}</a>
-                        </li>
-                </ul>
-              </div>
-            </nav>
-            <nav class="g-header__nav-secondary" aria-label="secondary">
-              <h2 class="sr-only" id="h-nav-secondary-header">Secondary Navigation</h2>
-              <ul class="f-secondary" aria-labelledby="h-nav-secondary-header">
-                    @guest
-                    @else
-                        <li class="dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-                        </li>
-                        <li>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+        <header class="g-header">
+            <div class="g-header__inner">
+                <a href="#content" class="skip-nav f-body">Skip to Content</a>
+                <nav aria-label="primary">
+                    <a class="g-header__logo" aria-label="Art Institute of Chicago" href="/">
+                        <svg aria-hidden="true">
+                            <use xlink:href="#icon--logo--80"></use>
+                            <use xlink:href="#icon--logo--88"></use>
+                            <use xlink:href="#icon--logo--92"></use>
+                        </svg>
+                    </a>
+                    <div class="g-header__nav-primary">
+                        <h2 class="sr-only" id="h-nav-primary-header">Primary Navigation</h2>
+                        <ul class="f-main-nav" aria-labelledby="h-nav-primary-header">
+                            <li>
+                                <a href="{{ route('doc-endpoints') }}">{{ __('Endpoints') }}</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('doc-fields') }}">{{ __('Fields') }}</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('doc-swagger') }}">{{ __('swagger.json') }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <nav class="g-header__nav-secondary" aria-label="secondary">
+                    <h2 class="sr-only" id="h-nav-secondary-header">Secondary Navigation</h2>
+                    <ul class="f-secondary" aria-labelledby="h-nav-secondary-header">
+                        @guest
+                        @else
+                            <li class="dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-              </ul>
-            </nav>
-            <p class="g-header__opening-hours f-secondary"><!-- message here --></p>
-            <button class="g-header__menu-link f-secondary" data-behavior="openNavMobile" aria-label="Show menu">Menu<svg class="icon--menu--24" aria-hidden="true"><use xlink:href="#icon--menu--24"></use></svg></button>
-        </div>
+                            </li>
+                            <li>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </nav>
+                <p class="g-header__opening-hours f-secondary"><!-- message here --></p>
+                <button class="g-header__menu-link f-secondary" data-behavior="openNavMobile" aria-label="Show menu">Menu<svg class="icon--menu--24" aria-hidden="true"><use xlink:href="#icon--menu--24"></use></svg></button>
+            </div>
         </header>
         <main class="content">
             <article class="o-article o-article--generic-page">
