aa0c97f2cd50b521c459a0bd901cd5f29bbff817
Illya Moskvin <imoskvin@artic.edu>
2020-04-06 09:25:59 -0500

    Refactor DimensionParser [WEB-76]
    Still not sure whether to keep it static or turn these into instance
    methods. Probably static makes more sense.

--- app/Library/DimensionParser.php
+++ app/Library/DimensionParser.php
@@ -11,59 +11,78 @@ class DimensionParser
      */
     public static function parse($dimensions)
     {
-        $dims;
-        $unit;
+        $result = self::processDimensionRules($dimensions);
 
-        if (preg_match('/[a-zA-Z():. ]*([0-9.]+) [x×] ([0-9.]+) [x×] ([0-9.]+) ([cmin]{2}).*/u', $dimensions, $matches)) {
+        return self::dimensionsInMillimeters([$result['h'], $result['w'], $result['d']], $result['u']);
+    }
 
-            $dims = array_slice($matches, 1, 3);
-            $unit = $matches[4];
-
+    private static function processDimensionRules($dimensions)
+    {
+        foreach (self::getDimensionRules() as $pattern => $callback) {
+            if (preg_match($pattern, $dimensions, $matches)) {
+                return $callback($matches);
+            }
         }
-        elseif (preg_match('/[a-zA-Z():. ]*([0-9.]+) [x×] ([0-9.]+) ([cmin]{2}).*/u', $dimensions, $matches)) {
-
-            $dims = array_slice($matches, 1, 2);
-            $unit = $matches[3];
-
-        }
-        elseif (preg_match('/[a-zA-Z():. ]*([0-9.]+) ([cmin]{2}) [()\/0-9a-z.; ]+ ([0-9.]+) [cmin]{2}.*/u', $dimensions, $matches)) {
-
-            $dims[] = $matches[1];
-            $dims[] = $matches[3];
-            $unit = $matches[2];
-
-        }
-        elseif (preg_match('/[a-zA-Z():. ]*([0-9.]+) ([cmin]{2}) [()\/0-9a-z.; ]+.*/u', $dimensions, $matches)) {
-
-            $dims[] = $matches[1];
-            $unit = $matches[2];
-
-        }
-
-        $dims = $dims ?? [0,0];
-        $unit = $unit ?? 'mm';
-
-        return self::dimensionsInMillimeters($dims, $unit);
-
     }
 
     private static function dimensionsInMillimeters($dims, $unit)
     {
-
         $ret = $dims;
+
         switch ($unit) {
-        case 'mm':
-            $ret = $dims;
-            break;
-        case 'cm':
-            $ret = array_map(function($dim) { return $dim * 10; }, $dims);
-            break;
-        case 'in':
-            $ret = array_map(function($dim) { return $dim * 25.4; }, $dims);
-            break;
+            case 'mm':
+                $ret = $dims;
+                break;
+            case 'cm':
+                $ret = array_map(function($dim) { return $dim * 10; }, $dims);
+                break;
+            case 'in':
+                $ret = array_map(function($dim) { return $dim * 25.4; }, $dims);
+                break;
         }
 
         return array_pad(array_map('intval', $ret), 3, null);
-
     }
+
+    /**
+     * Defined dynamically to avoid "Constant expression contains invalid operations" fatal error.
+     */
+    private static function getDimensionRules()
+    {
+        return [
+            '/[a-zA-Z():. ]*([0-9.]+) [x×] ([0-9.]+) [x×] ([0-9.]+) ([cmin]{2}).*/u' => function ($match) {
+                return [
+                    'h' => $match[1],
+                    'w' => $match[2],
+                    'd' => $match[3],
+                    'u' => $match[4],
+                ];
+            },
+            '/[a-zA-Z():. ]*([0-9.]+) [x×] ([0-9.]+) ([cmin]{2}).*/u' => function ($match) {
+                return [
+                    'h' => $match[1],
+                    'w' => $match[2],
+                    'd' => null,
+                    'u' => $match[3],
+                ];
+            },
+            '/[a-zA-Z():. ]*([0-9.]+) ([cmin]{2}) [()\/0-9a-z.; ]+ ([0-9.]+) [cmin]{2}.*/u' => function ($match) {
+                return [
+                    'h' => $match[1],
+                    'w' => $match[3],
+                    'd' => null,
+                    'u' => $match[2],
+                ];
+            },
+            '/[a-zA-Z():. ]*([0-9.]+) ([cmin]{2}) [()\/0-9a-z.; ]+.*/u' => function ($match) {
+                return [
+                    'h' => $match[1],
+                    'w' => null,
+                    'd' => null,
+                    'u' => $match[2],
+                ];
+            },
+        ];
+    }
+
 }
