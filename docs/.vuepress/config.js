4e6954e5685dc520663f2dcc6072952d3feac708
nikhiltri <ntrivedi@artic.edu>
2020-07-31 19:23:34 -0500

    Add link to swagger.json in header [WEB-1767]

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -49,6 +49,10 @@ module.exports = {
       {
         text: 'Tutorials',
         link: '/tutorials/'
+      },
+      {
+        text: 'swagger.json',
+        link: 'https://api.artic.edu/api/v1/swagger.json'
       }
     ]
   },
