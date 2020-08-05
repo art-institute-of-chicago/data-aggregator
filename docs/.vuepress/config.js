7800813a4013f341b0bc539ca2519ebcb2387b53
Illya Moskvin <ivmoskvin@gmail.com>
2020-08-05 12:49:24 -0500

    Change base URL in Vuepress back to `/docs/` [WEB-1767]

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const { description } = require('../../package')
 
 module.exports = {
-  base: '/data-aggregator/',
+  base: '/docs/',
 
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
