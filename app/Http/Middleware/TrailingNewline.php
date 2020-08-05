47756d9224b8b0d6108c5cf944c5cd3b7c098690
Illya Moskvin <ivmoskvin@gmail.com>
2020-08-05 16:35:40 -0500

    Don't run TrailingNewline on StreamedResponse

--- app/Http/Middleware/TrailingNewline.php
+++ app/Http/Middleware/TrailingNewline.php
@@ -3,12 +3,18 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class TrailingNewline
 {
     public function handle($request, Closure $next)
     {
         $response = $next($request);
+
+        if ($response instanceof StreamedResponse) {
+            return $response;
+        }
+
         $content = $response->getContent();
 
         if (substr($content, -1) !== "\n") {
