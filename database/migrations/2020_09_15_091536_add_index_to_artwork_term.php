e8f64905a85c1482d59d8b8c16e13bbdb6edc6eb
nikhiltri <ntrivedi@artic.edu>
2020-09-15 09:18:03 -0500

    Add index to v1_artwork_term

---
+++ database/migrations/2020_09_15_091536_add_index_to_artwork_term.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexToArtworkTerm extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('artwork_term', function (Blueprint $table) {
+            $table->index(['artwork_citi_id', 'term_lake_uid']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('artwork_term', function (Blueprint $table) {
+            $table->dropIndex(['artwork_citi_id', 'term_lake_uid']);
+        });
+    }
+}
