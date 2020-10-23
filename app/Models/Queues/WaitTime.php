84a44a8637849f002cd2d91f5cb068f08cfa55bd
nikhiltri <ntrivedi@artic.edu>
2020-10-23 11:02:05 -0500

    Removed  El Greco queues

--- app/Models/Queues/WaitTime.php
+++ app/Models/Queues/WaitTime.php
@@ -11,8 +11,6 @@ class WaitTime extends BaseModel
 {
 
     public static $aicQueueIds = [
-        1980 => 'El Greco – Member',
-        1981 => 'El Greco – GA',
         1982 => 'Monet – Member',
         1983 => 'Monet – GA',
     ];
