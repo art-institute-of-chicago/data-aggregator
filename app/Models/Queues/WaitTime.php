724e2e9ce2fc6e5ef6311555779b84c59c61718f
Illya Moskvin <ivmoskvin@gmail.com>
2020-11-18 10:34:01 -0600

    Add Bisa Butler queues [WEB-1889]

--- app/Models/Queues/WaitTime.php
+++ app/Models/Queues/WaitTime.php
@@ -11,6 +11,8 @@ class WaitTime extends BaseModel
 {
 
     public static $aicQueueIds = [
+        1980 => 'Bisa Butler – Member',
+        1981 => 'Bisa Butler – GA',
         1982 => 'Monet – Member',
         1983 => 'Monet – GA',
     ];
