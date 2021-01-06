4a05853eed0946a72262d6b2da51bd1ee837a691
Illya Moskvin <imoskvin@artic.edu>
2021-01-06 16:41:40 -0600

    Fix artwork manifests after NetX migration [WEB-1902]

--- app/Transformers/Outbound/Collections/ArtworkManifest.php
+++ app/Transformers/Outbound/Collections/ArtworkManifest.php
@@ -5,6 +5,7 @@ namespace App\Transformers\Outbound\Collections;
 use League\Fractal\TransformerAbstract as BaseTransformer;
 
 use App\Models\Collections\Artwork;
+use App\Models\Collections\Asset;
 use App\Transformers\Outbound\Collections\Traits\IsCC0;
 
 class ArtworkManifest extends BaseTransformer
@@ -19,11 +20,13 @@ class ArtworkManifest extends BaseTransformer
         if ($model->image) {
             $canvases[] = $this->_createCanvasImage($model, $model->image);
         }
+
         if ($model->altImages) {
             foreach ($model->altImages as $image) {
                 $canvases[] = $this->_createCanvasImage($model, $image);
             }
         }
+
         return [
             '@context' => 'http://iiif.io/api/presentation/2/context.json',
             '@id' => config('app.url') . '/api/v1/artworks/' . $model->citi_id . '/manifest.json',
@@ -76,33 +79,27 @@ class ArtworkManifest extends BaseTransformer
 
     private function _createCanvasImage($model, $image)
     {
-        $jsonData = \Cache::remember('info-json-'.$image->lake_guid, 86400, function () use ($image) {
-            return @file_get_contents(config('aic.config_documentation.website_url') . '/iiif/2/' . $image->lake_guid . '/info.json');
-        });
-        if (!$jsonData) {
-            return;
-        }
-        $imageData = json_decode($jsonData);
+        $imageUuid = Asset::getHashedId($image->lake_guid);
 
         return [
             '@type' => 'sc:Canvas',
-            '@id' => config('aic.config_documentation.iiif_url') . '/' . $image->lake_guid,
+            '@id' => config('aic.config_documentation.iiif_url') . '/' . $imageUuid,
             'label' => strip_tags($model->title.', '.$model->date_display.'. '.str_replace("\n", ', ', $model->artist_display)),
-            'width' => $imageData->width,
-            'height' => $imageData->height,
+            'width' => $image->width,
+            'height' => $image->height,
             'images' => [
                 [
                     '@type' => 'oa:Annotation',
                     'motivation' => 'sc:painting',
-                    'on' => config('aic.config_documentation.iiif_url') . '/' . $image->lake_guid,
+                    'on' => config('aic.config_documentation.iiif_url') . '/' . $imageUuid,
                     'resource' => [
                         '@type' => 'dctypes:Image',
-                        '@id' => config('aic.config_documentation.iiif_url') . '/' . $image->lake_guid . '/full/full/0/default.jpg',
-                        'width' => $imageData->width,
-                        'height' => $imageData->height,
+                        '@id' => config('aic.config_documentation.iiif_url') . '/' . $imageUuid . '/full/full/0/default.jpg',
+                        'width' => $image->width,
+                        'height' => $image->height,
                         'service' => [
                             '@context' => 'http://iiif.io/api/image/2/context.json',
-                            '@id' => config('aic.config_documentation.iiif_url') . '/' . $image->lake_guid,
+                            '@id' => config('aic.config_documentation.iiif_url') . '/' . $imageUuid,
                             'profile' => 'http://iiif.io/api/image/2/level2.json'
                         ]
                     ]
