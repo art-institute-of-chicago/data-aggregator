3d9229b9b91abf7067ae29e45c2f2bba1bb7c9b4
Illya Moskvin <imoskvin@artic.edu>
2021-04-22 22:27:38 -0500

    Disable `dump:monthly` to prevent incomplete dumps [WEB-2013]

--- app/Console/Commands/Dump/DumpMonthly.php
+++ app/Console/Commands/Dump/DumpMonthly.php
@@ -14,13 +14,25 @@ class DumpMonthly extends AbstractDumpCommand
 
     public function handle()
     {
+        // WEB-2013: Hotifx to prevent this from running, for the moment!
+        return;
+
         $this->info('Dumping getting started...');
         $this->call('dump:getting-started');
 
+        // WEB-2013: `dump:export`
         $this->info('Dumping all...');
         $this->call('dump:export');
 
-        $this->info('Uploading to GitHub...');
-        $this->call('dump:upload');
+        /**
+         * WEB-2013: `dump:export` exits immediately after launching screens.
+         * If we allow this to run, it will upload an incomplete dump to GitHub!
+         *
+         * Currently, we should call `dump:export` manually, and manually check
+         * that all of the screens have exited before uploading a dump. This
+         * verification process should be automated before we enable this.
+         */
+        // $this->info('Uploading to GitHub...');
+        // $this->call('dump:upload');
     }
 }
