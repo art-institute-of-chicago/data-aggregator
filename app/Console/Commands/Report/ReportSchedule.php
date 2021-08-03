c30b2d9d07e3ff6a057673b20cb612acb7bdd9e2
Illya Moskvin <ivmoskvin@gmail.com>
2021-08-03 12:40:19 -0500

    Add `report:schedule` command [API-190]

---
+++ app/Console/Commands/Report/ReportSchedule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands\Report;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Aic\Hub\Foundation\AbstractCommand as BaseCommand;
+
+class ReportSchedule extends BaseCommand
+{
+
+    protected $signature = 'report:schedule';
+
+    protected $description = 'Show which scheduled tasks are currently running';
+
+    public function handle()
+    {
+        $events = app(Schedule::class)->events();
+
+        foreach ($events as $event) {
+            $command = implode(' ', array_slice(explode(' ', $event->command), 2));
+            $this->info($command . ' = ' . var_export($event->mutex->exists($event), true));
+        }
+    }
+}
