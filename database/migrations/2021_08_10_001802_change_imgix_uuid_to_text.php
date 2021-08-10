7dabee6978acfa622cb6d9f0e59fb174e4a9714f
Illya Moskvin <imoskvin@artic.edu>
2021-08-10 00:23:47 -0500

    Change `imgix_uuid` column to text [API-192]

---
+++ database/migrations/2021_08_10_001802_change_imgix_uuid_to_text.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeImgixUuidToText extends Migration
+{
+    private $tableNames = [
+        'articles',
+        'generic_pages',
+        'press_releases',
+        'educator_resources',
+        'digital_catalogs',
+        'printed_catalogs',
+    ];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach ($this->tableNames as $tableName) {
+            Schema::table($tableName, function (Blueprint $table) {
+                $table->text('imgix_uuid')->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        foreach ($this->tableNames as $tableName) {
+            Schema::table($tableName, function (Blueprint $table) {
+                $table->string('imgix_uuid')->change();
+            });
+        }
+    }
+}
