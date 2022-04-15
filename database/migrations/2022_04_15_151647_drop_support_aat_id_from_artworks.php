4e26a6011abdf1af169b22db3277ebf87f241874
Illya Moskvin <imoskvin@artic.edu>
2022-04-15 15:19:51 -0500

    Remove `support_aat_id` from artworks [API-287]

---
+++ database/migrations/2022_04_15_151647_drop_support_aat_id_from_artworks.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropSupportAatIdFromArtworks extends Migration
+{
+    public function up()
+    {
+        Schema::table('artworks', function (Blueprint $table) {
+            $table->dropColumn([
+                'support_aat_id',
+            ]);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('artworks', function (Blueprint $table) {
+            $table->integer('support_aat_id')->signed()->nullable()->after('medium_display');
+        });
+    }
+}
