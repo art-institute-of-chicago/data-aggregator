037a7fb26e849af98044867c9baf531d1a250c4c
Illya Moskvin <imoskvin@artic.edu>
2022-07-12 14:49:46 -0500

    Fix `source_updated_at` to timestamp [API-306]

---
+++ database/migrations/2022_07_12_141741_fix_source_updated_at_to_timestamp.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixSourceUpdatedAtToTimestamp extends Migration
+{
+    private $tableNames = [
+        'agent_place_qualifiers',
+        'agent_roles',
+        'agent_types',
+        'agents',
+        'articles',
+        'artwork_date_qualifiers',
+        'artwork_place_qualifiers',
+        'artwork_types',
+        'artworks',
+        'assets',
+        'catalogues',
+        'category_terms',
+        'digital_catalogs',
+        'educator_resources',
+        'event_occurrences',
+        'event_programs',
+        'events',
+        'exhibitions',
+        'experiences',
+        'galleries',
+        'generic_pages',
+        'highlights',
+        'interactive_features',
+        'issue_articles',
+        'issues',
+        'places',
+        'press_releases',
+        'printed_catalogs',
+        'products',
+        'ticketed_event_types',
+        'ticketed_events',
+        'web_artists',
+        'web_exhibitions',
+    ];
+
+    public function up()
+    {
+        foreach ($this->tableNames as $tableName) {
+            Schema::table($tableName, function (Blueprint $table) {
+                $table->timestamp('source_updated_at')->nullable()->default(null)->change();
+            });
+        }
+    }
+
+    public function down()
+    {
+        foreach ($this->tableNames as $tableName) {
+            Schema::table($tableName, function (Blueprint $table) {
+                $table->dateTime('source_updated_at')->nullable()->default(null)->change();
+            });
+        }
+    }
+}
