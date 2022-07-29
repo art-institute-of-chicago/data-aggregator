2c6f0eb5037048b5bf99580c7bd5b5d41ac7e4dc
Illya Moskvin <imoskvin@artic.edu>
2022-07-28 22:27:43 -0500

    Add index on `agent_id` to `web_artists` [API-341, API-336]

--- database/migrations/2022_07_28_221343_clean_web_artists.php
+++ database/migrations/2022_07_28_221343_clean_web_artists.php
@@ -10,12 +10,14 @@ class CleanWebArtists extends Migration
     {
         Schema::table('web_artists', function (Blueprint $table) {
             $table->renameColumn('datahub_id', 'agent_id');
+            $table->index('agent_id');
         });
     }
 
     public function down()
     {
         Schema::table('web_artists', function (Blueprint $table) {
+            $table->dropIndex(['agent_id']);
             $table->renameColumn('agent_id', 'datahub_id');
         });
     }
