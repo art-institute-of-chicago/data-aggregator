a3d1eb21d73c9589fb01fbcc09a8b67582e44c69
nikhiltri <ntrivedi@artic.edu>
2023-05-11 13:41:07 -0500

    Get latest aic/laravel-scout-elastic and upgrade laravel/scout to 9.3 [WEB-1388]

--- config/scout.php
+++ config/scout.php
@@ -11,7 +11,7 @@ return [
     | using Laravel Scout. This connection is used when syncing all models
     | to the search service. You should adjust this based on your needs.
     |
-    | Supported: "algolia", "null"
+    | Supported: "algolia", "meilisearch", "collection", "null"
     |
     */
 
@@ -45,6 +45,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Database Transactions
+    |--------------------------------------------------------------------------
+    |
+    | This configuration option determines if your data will only be synced
+    | with your search indexes after every open database transaction has
+    | been committed, thus preventing any discarded data from syncing.
+    |
+    */
+
+    'after_commit' => false,
+
+    /*
+    |--------------------------------------------------------------------------
     | Chuck Sizes
     |--------------------------------------------------------------------------
     |
@@ -55,9 +68,37 @@ return [
     */
 
     'chunk' => [
-        'searchable' => 100,
-        'unsearchable' => 100,
+        'searchable' => 500,
+        'unsearchable' => 500,
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Soft Deletes
+    |--------------------------------------------------------------------------
+    |
+    | This option allows to control whether to keep soft deleted records in
+    | the search indexes. Maintaining soft deleted records can be useful
+    | if your application still needs to search for the records later.
+    |
+    */
+
+    'soft_delete' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Identify User
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to control whether to notify the search engine
+    | of the user performing the search. This is sometimes useful if the
+    | engine supports any analytics based on this application's users.
+    |
+    | Supported engines: "algolia"
+    |
+    */
+
+    'identify' => env('SCOUT_IDENTIFY', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -73,6 +114,24 @@ return [
     'algolia' => [
         'id' => env('ALGOLIA_APP_ID', ''),
         'secret' => env('ALGOLIA_SECRET', ''),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | MeiliSearch Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your MeiliSearch settings. MeiliSearch is an open
+    | source search engine with minimal configuration. Below, you can state
+    | the host and key information for your own MeiliSearch installation.
+    |
+    | See: https://docs.meilisearch.com/guides/advanced_guides/configuration.html
+    |
+    */
+
+    'meilisearch' => [
+        'host' => env('MEILISEARCH_HOST', 'http://localhost:7700'),
+        'key' => env('MEILISEARCH_KEY', null),
     ],
 
     /*
