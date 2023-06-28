318bc9c4a6596900c8a4b4edc68cb53fca0fe971
nikhiltri <ntrivedi@artic.edu>
2023-06-28 15:50:52 -0500

    Add mutliple IDs request to linked art endpoint [API-378]

--- routes/la.php
+++ routes/la.php
@@ -6,5 +6,6 @@ app('url')->forceScheme(config('aic.proxy_scheme'));
 use App\Http\Controllers\LinkedArtController;
 
 Route::group(['prefix' => 'v1'], function () {
+    Route::get('objects', [LinkedArtController::class, 'showMultiple'])->name('ld.objects');
     Route::get('objects/{id}', [LinkedArtController::class, 'showObject'])->name('ld.object');
 });
