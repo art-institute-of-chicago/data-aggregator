f13cb94e174e55799d805981433265d973e5f6f4
nikhiltri <ntrivedi@artic.edu>
2023-07-10 13:27:00 -0500

    Add search endpoint to linked art endpoint [API-380]

---
+++ app/Http/Controllers/LinkedArtSearchController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Aic\Hub\Foundation\Exceptions\DetailedException;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\SearchController as BaseController;
+
+class LinkedArtSearchController extends BaseController
+{
+    public function search(Request $request, $resource = null)
+    {
+        $resource = $resource ?? 'artworks';
+
+        $searchResponse = $this->query(
+            'getSearchParams',
+            'getSearchResponse',
+            'search',
+            $resource,
+            null,
+            [
+                'boost' => false,
+                'fields' => [
+                    'id',
+                    'title',
+                    'date_display',
+                    'thumbnail',
+                    'image_id',
+                    'api_model',
+                    'artist_pivots',
+                    'artist_title',
+                    'artist_display',
+                    'main_reference_number',
+                    'is_public_domain',
+                    'is_on_view',
+                    'department_title',
+                    'date_start',
+                    'date_end',
+                    'place_of_origin',
+                    'medium_display',
+                ],
+            ],
+        );
+
+        $artworks = $searchResponse['data'];
+
+        $artworkIds = collect($artworks)
+            ->pluck('id')
+            ->unique()
+            ->values()
+            ->all();
+
+        // Returns multi-id response with empty `data`
+        if (empty($artworkIds)) {
+            $artworkIds = [
+                1, // almost certainly will never exist
+            ];
+        }
+
+        $path = '/la/v1/objects';
+
+        // Get ServerBag of current reqest, rewrite URI, convert to array
+        $server = request()->server;
+        $server->set('REQUEST_URI', $path);
+        $server = $server->all();
+
+        // Rewrite the current request with one containing new URI and JSON
+        $request = new Request(['ids' => implode(',', $artworkIds)], [], [], [], [], $server, json_encode([
+            'fields' => $request->fields ?? [],
+        ]));
+
+        app()->request = $request;
+
+        // Now, calling request() returns the new request!
+        $artworkResponse = Route::dispatch($request)->getContent();
+
+        return response($artworkResponse)->header('Content-Type', 'application/json');
+    }
+}
--- routes/la.php
+++ routes/la.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\LinkedArtController;
+use App\Http\Controllers\LinkedArtSearchController;
+
 app('url')->forceRootUrl(config('aic.proxy_url'));
 app('url')->forceScheme(config('aic.proxy_scheme'));
 
-use App\Http\Controllers\LinkedArtController;
-
 Route::group(['prefix' => 'v1'], function () {
-    Route::get('objects', [LinkedArtController::class, 'showMultiple'])->name('ld.objects');
+    Route::match(['GET', 'POST'], 'objects/search', [LinkedArtSearchController::class, 'search'])->name('la.objects.search');
     Route::get('objects/{id}', [LinkedArtController::class, 'showObject'])->name('ld.object');
+    Route::get('objects', [LinkedArtController::class, 'showMultiple'])->name('ld.objects');
 });
