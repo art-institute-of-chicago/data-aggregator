389ef5964787bbb1050b2ebbd1b003d913e5af2f
nikhiltri <ntrivedi@artic.edu>
2023-09-06 15:29:45 -0500

    Import `nomisma_id` from enhancer explicitly [API-387]

--- app/Transformers/Inbound/Enhancer/Artwork.php
+++ app/Transformers/Inbound/Enhancer/Artwork.php
@@ -13,6 +13,7 @@ class Artwork extends BaseTransformer
             'dimension_width' => $datum->width,
             'dimension_height' => $datum->height,
             'linked_art_json' => $datum->linked_art_json,
+            'nomisma_id' => $datum->nomisma_id,
         ];
     }
 }
