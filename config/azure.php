2a96119b651526e6a6b911e0c2927bc10fd04716
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2024-11-26 11:45:46 -0600

    Update azure config with image_embedding and versioning [WEB-2934]

--- config/azure.php
+++ config/azure.php
@@ -14,14 +14,22 @@ return [
     'embedding' => [
         'endpoint' => env(key: 'AZURE_EMBEDDING_API_ENDPOINT'),
         'key' => env(key: 'AZURE_EMBEDDING_API_KEY'),
+        'version' => env(key: 'AZURE_EMBEDDING_API_VERSION')
+    ],
+    'image_embedding' => [
+        'endpoint' => env(key: 'AZURE_IMAGE_EMBEDDING_API_ENDPOINT'),
+        'key' => env(key: 'AZURE_IMAGE_EMBEDDING_API_KEY'),
+        'version' => env(key: 'AZURE_IMAGE_EMBEDDING_API_VERSION'),
     ],
     'image_analysis' => [
         'endpoint' => env(key: 'AZURE_IMAGE_ANALYSIS_API_ENDPOINT'),
         'key' => env(key: 'AZURE_IMAGE_ANALYSIS_API_KEY'),
+        'version' => env(key: 'AZURE_IMAGE_ANALYSIS_API_VERSION'),
     ],
     'completion' => [
         'endpoint' => env(key: 'AZURE_COMPLETIONS_API_ENDPOINT'),
         'key' => env(key: 'AZURE_COMPLETIONS_API_KEY'),
+        'version' => env(key: 'AZURE_COMPLETIONS_API_VERSION'),
     ],
 
 ];
