bc8bdaf374a94016ebfbfabbc13c544674cb11e5
nikhiltri <ntrivedi@artic.edu>
2024-12-02 14:37:25 -0600

    Add database migrations for vector tables [WEB-2934]

---
+++ database/migrations/2022_08_03_000000_create_vector_extension.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function __construct()
+    {
+        $this->connection = 'vectors';
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('CREATE EXTENSION IF NOT EXISTS vector');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP EXTENSION vector');
+    }
+};
