2945ec7783aa812edcd824e1e512c42833d2a6ec
nikhiltri <ntrivedi@artic.edu>
2024-12-19 09:09:21 -0600

    Set default Azure API versions [WEB-2934]

--- config/azure.php
+++ config/azure.php
@@ -15,22 +15,22 @@ return [
     'embedding' => [
         'endpoint' => env(key: 'AZURE_EMBEDDING_API_ENDPOINT'),
         'key' => env(key: 'AZURE_EMBEDDING_API_KEY'),
-        'version' => env(key: 'AZURE_EMBEDDING_API_VERSION')
+        'version' => env(key: 'AZURE_EMBEDDING_API_VERSION', default: '2023-05-15')
     ],
     'image_embedding' => [
         'endpoint' => env(key: 'AZURE_IMAGE_EMBEDDING_API_ENDPOINT'),
         'key' => env(key: 'AZURE_IMAGE_EMBEDDING_API_KEY'),
-        'version' => env(key: 'AZURE_IMAGE_EMBEDDING_API_VERSION'),
+        'version' => env(key: 'AZURE_IMAGE_EMBEDDING_API_VERSION', default: '2023-04-15'),
     ],
     'image_analysis' => [
         'endpoint' => env(key: 'AZURE_IMAGE_ANALYSIS_API_ENDPOINT'),
         'key' => env(key: 'AZURE_IMAGE_ANALYSIS_API_KEY'),
-        'version' => env(key: 'AZURE_IMAGE_ANALYSIS_API_VERSION'),
+        'version' => env(key: 'AZURE_IMAGE_ANALYSIS_API_VERSION', default: '2024-02-01'),
     ],
     'completion' => [
         'endpoint' => env(key: 'AZURE_COMPLETIONS_API_ENDPOINT'),
         'key' => env(key: 'AZURE_COMPLETIONS_API_KEY'),
-        'version' => env(key: 'AZURE_COMPLETIONS_API_VERSION'),
+        'version' => env(key: 'AZURE_COMPLETIONS_API_VERSION', default: '2024-08-01'),
     ],
 
 ];
