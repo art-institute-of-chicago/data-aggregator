a64592d8af19082c35de271ba93684f2c780198d
nikhiltri <ntrivedi@artic.edu>
2025-06-04 15:35:53 -0500

    Cast `is_sales_button_hidden` to boolean [MA-170]

--- app/Models/Web/EventOccurrence.php
+++ app/Models/Web/EventOccurrence.php
@@ -15,6 +15,7 @@ class EventOccurrence extends WebModel
     protected $casts = [
         'is_private' => 'boolean',
         'is_ticketed' => 'boolean',
+        'is_sales_button_hidden' => 'boolean',
         'start_at' => 'datetime',
         'end_at' => 'datetime',
     ];
