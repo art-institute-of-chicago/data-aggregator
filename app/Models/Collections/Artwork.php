753ac5036bc1c443bcbf5f200043a33714775c14
nikhiltri <ntrivedi@artic.edu>
2026-02-27 17:29:46 -0600

    Uncomment the code meant to fix vector search [ART-89]

--- app/Models/Collections/Artwork.php
+++ app/Models/Collections/Artwork.php
@@ -72,11 +72,11 @@ class Artwork extends CollectionsModel
         ];
 
         if (!config('aic.search.suppress_vector_search')) {
-            // $ret['bool']['should'] = [
-            //     'exists' => [
-            //         'field' => 'text_embedding'
-            //     ]
-            // ];
+            $ret['bool']['should'] = [
+                'exists' => [
+                    'field' => 'text_embedding'
+                ]
+            ];
         }
 
         return $ret;
