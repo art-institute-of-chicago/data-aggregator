25dfd363331639ae4d144c89335fd20478bca80e
nikhiltri <nikhiltri@users.noreply.github.com>
2026-08-27 19:41:55 +0000

    deploy: ac27b1edbc57a7c931b4a5b8dc8c73012ff64386

---
+++ assets/js/23.e50b12e9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{350:function(n,s,t){"use strict";t.r(s);var e=t(19),o=Object(e.a)({},(function(){return(0,this._self._c)("p",{staticClass:"demo"},[this._v("This is another component")])}),[],!1,null,null,null);s.default=o.exports}}]);
