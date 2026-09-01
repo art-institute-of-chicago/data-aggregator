1857f5deba4ed3238823261d0da37eb28c02810f
nikhiltri <ntrivedi@artic.edu>
2026-09-01 13:28:16 -0500

    List code [API-465]

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,9 +8,9 @@ use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         then: function () {
             Route::middleware('ai.service.status')
