2acdb3e902879f001599c72b5e34a763295cf3f9
nikhiltri <ntrivedi@artic.edu>
2026-09-02 17:11:53 -0500

    Code changes to stay up to date with Laravel 13 [API-464]

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ $config = [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ], fn ($value) => $value !== null) : [],
         ],
 
@@ -80,7 +80,7 @@ $config = [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ], fn ($value) => $value !== null) : [],
         ],
 
--- database/migrations/2026_09_02_115707_create_cache_locks_table.php
+++ database/migrations/2026_09_02_115707_create_cache_locks_table.php
@@ -13,7 +13,7 @@ return new class () extends Migration {
         Schema::create('cache_locks', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->string('owner');
-            $table->integer('expiration')->index();
+            $table->bigInteger('expiration')->index();
         });
     }
 
