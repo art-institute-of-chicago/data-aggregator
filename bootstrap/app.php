d08980b0168ad1797ce3c1228896c4acb6a1921e
WebDevTrev <96503752+web-dev-trev@users.noreply.github.com>
2026-09-03 15:16:15 -0500

    Schedule counting daily [ART-102]

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\Route;
 use Sentry\Laravel\Integration;
 
@@ -88,4 +89,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->reportable(function (Throwable $e) {
             Integration::captureUnhandledException($e);
         });
+    })
+    ->withSchedule(function (Schedule $schedule): void {
+        $schedule->command('report:category-terms')
+            ->daily()
+            ->withoutOverlapping();
     })->create();
