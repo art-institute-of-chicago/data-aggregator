85e4f64baf7b96d74de6fe2a7a547e4cb8ddee41
nikhiltri <ntrivedi@artic.edu>
2026-09-22 10:18:02 -0500

    Add logging to Sentry [API-469]

--- config/logging.php
+++ config/logging.php
@@ -122,5 +122,12 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'sentry_logs' => [
+            'driver' => 'sentry_logs',
+            // The minimum logging level at which this handler will be triggered
+            // Available levels: debug, info, notice, warning, error, critical, alert, emergency
+            'level' => env('LOG_LEVEL', 'info'), // defaults to `debug` if not set
+        ],
+
     ],
 ];
